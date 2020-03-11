83ba44f47253e81fd446a7322255001c154d1ebc
Jessica Liang <jessicursliang@gmail.com>
2020-03-10 19:42:37 -0700

    updated testing for Recipient

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -1,10 +1,15 @@
 require_relative 'test_helper'
 
-describe "" do
-	
-	it "" do
-		
+describe "initalize" do
 
+	before do
+		VCR.use_cassette("recipient_create") do
+			@response = SlackCLI::Recipient.get_response("users.list")
+		end
+	end
+	
+	it "returns a hash" do
+		expect(@response).must_be_instance_of HTTParty::Response
 	end
 
 end
