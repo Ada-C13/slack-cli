142692670d12d9b0f41d4d904c7843cf6742cfc1
Jocelyn Wang <wangjoc@gmail.com>
2020-03-11 11:05:35 -0700

    moved list all channel code to Channel class

--- lib/channel.rb
+++ lib/channel.rb
@@ -3,10 +3,32 @@ require_relative 'recipient'
 class Channel < Recipient
   attr_reader :name, :topic, :member_count, :id
 
+  CHANNEL_URL = 'https://slack.com/api/channels.list'
+  MEMBER_URL = 'https://slack.com/api/users.list'
+  TOKEN = ENV['SLACK_TOKEN']
+
   def initialize(name:,topic:,member_count:,id:)
     super(name, id)
     @topic = topic
     @member_count = member_count
+  end
+
+  # username, real name, and Slack ID
+  def self.all
+    query_parameters = {
+      token: TOKEN
+    }
+
+    response = HTTParty.get(CHANNEL_URL, query: query_parameters)["channels"]
+  
+    response.map {|channel| 
+      Channel.new(
+        name: channel["name"],
+        topic: channel["topic"]["value"],
+        member_count: channel["members"].length,
+        id: channel["id"]
+      )
+    }
   end
 
 end
--- lib/workspace.rb
+++ lib/workspace.rb
@@ -10,30 +10,9 @@ Dotenv.load
 class Workspace
   attr_reader :users, :channels
 
-  CHANNEL_URL = 'https://slack.com/api/channels.list'
-  MEMBER_URL = 'https://slack.com/api/users.list'
-  TOKEN = ENV['SLACK_TOKEN']
-
   def initialize
     @users = Member.all
-    @channels = list_channels
+    @channels = Channel.all
   end
 
-# channel's name, topic, member count, and Slack ID.
-  def list_channels
-    query_parameters = {
-      token: TOKEN
-    }
-
-    response = HTTParty.get(CHANNEL_URL, query: query_parameters)["channels"]
-    
-    response.map {|channel| 
-      Channel.new(
-        name: channel["name"],
-        topic: channel["topic"]["value"],
-        member_count: channel["members"].length,
-        id: channel["id"]
-      )
-    }
-  end
 end
