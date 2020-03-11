8bd406d199a155683547e17ad96f1778be5cfb16
Rosalyn <syrosalynyu@gmail.com>
2020-03-11 16:30:42 -0700

    Added the wave 1 interactive pieces in slack.rb
    Three options, including list users, list channels, and quit.
    
    After completing any command other than quit, the program should reprint the list of commands and ask for another input.

--- lib/slack.rb
+++ lib/slack.rb
@@ -8,7 +8,24 @@ Dotenv.load
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
-  # TODO project
+  # Print users/channels
+  puts "3 options: list users, list channels, or quit:"
+  input = gets.chomp
+  
+  while input != "quit"
+    # puts "you didn't quit"
+    if input == "list users"
+      puts workspace.users
+    elsif input == "list channels"
+      puts workspace.channels
+    else 
+      puts "try again"
+    end
+    puts "3 options: list users, list channels, or quit:"
+    input = gets.chomp
+  end
+
+
 
   puts "Thank you for using the Ada Slack CLI"
 end
--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,10 +1,17 @@
+require "httparty"
+require "dotenv"
+require "awesome_print"
+
+require_relative "channel"
+require_relative "user"
+
 class Workspace
 
   attr_reader :users, :channels
 
   def initialize
-    @users = [] # arrays of users
-    @channels = [] # arrays of channels
+    @users = User.list_all # arrays of users
+    @channels = Channel.list_all # arrays of channels
   end
 
 end
