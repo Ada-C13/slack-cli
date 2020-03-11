cdae88e2b0ebed0a856eadb124e84fdf1e89ba5b
Maryam Heshmati <mairheshmati@gmail.com>
2020-03-10 22:24:08 -0700

    finished wave 1 of project- user can choose from three options <list channels\nlist users\nquit>- this class is responsible for interacting with the user

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,12 +1,35 @@
 #!/usr/bin/env ruby
+require "httparty"
+require "dotenv"
+require "awesome_print"
+require_relative "workspace"
 
 def main
-  puts "Welcome to the Ada Slack CLI!"
+  puts "Welcome to the Mair Bear's Slack CLI!"
   workspace = Workspace.new
+  puts "There are #{workspace.channels.length} channels and #{workspace.users.length} users in this Slack."
 
-  # TODO project
+  while true
+    puts "What would you like to do?"
+    puts "\nlist users\nlist channels\nquit"
+    options = ["list users", "list channels", "quit"]
+    users_option = gets.chomp.downcase
+    until options.include? users_option
+      puts "Please input a valid command."
+      users_option = gets.chomp.downcase
+    end
 
-  puts "Thank you for using the Ada Slack CLI"
+    case users_option
+    when "list users"
+      ap workspace.users
+    when "list channels"
+      ap workspace.channels
+    when "quit"
+      break
+    end
+  end
+
+  puts "Thank you for using the Mair Bear's Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
