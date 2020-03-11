5211162a7a70c9bfe2bde07e2d7fb8285b39a943
Catherina <catherine.pchelina@gmail.com>
2020-03-10 22:05:45 -0700

    Add Workspace class

---
+++ lib/workspace.rb
@@ -0,0 +1,12 @@
+require_relative "user"
+require_relative "channel"
+
+class Workspace
+  attr_reader :users, :channels
+
+  def initialize
+    @users = User.list_all
+    @channels = Channel.list_all
+  end
+
+end
