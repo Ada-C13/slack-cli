32cd0d6c9de1c5139c3864f300548f20084b5def
Jessica Liang <jessicursliang@gmail.com>
2020-03-10 22:15:39 -0700

    added ability for user to choose what to do

--- lib/slack.rb
+++ lib/slack.rb
@@ -2,6 +2,7 @@
 
 require 'dotenv'
 require 'httparty'
+require 'table_print'
 
 require_relative 'workspace'
 require_relative 'channel'
@@ -10,12 +11,28 @@ require_relative 'user'
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = SlackCLI::Workspace.new
-
   puts "#{ workspace.channels.length } channels loaded.."
   puts "#{ workspace.users.length } users loaded.."
-  puts "What would you like to do ?\n1. List users\n2. List channels\n3. Quit"
+  
+  continue = true
+  until continue == false
+    puts "What would you like to do ?\n1. List users\n2. List channels\n3. Quit"
+    choice = gets.chomp.downcase
 
-  # TODO project
+    if ["1", "list users"].include?(choice)
+      puts "*"
+      tp workspace.users, :username
+      puts "*"
+    elsif ["2", "list channels"].include?(choice)
+      puts "*"
+      tp workspace.channels, :name
+      puts "*"
+    elsif ["3", "quit", "q"].include?(choice)
+      continue = false
+    else
+      puts "Please choice an option above."
+    end
+  end
 
   puts "Thank you for using the Ada Slack CLI"
 end
--- test/slack_test.rb
+++ test/slack_test.rb
@@ -1,10 +1,11 @@
 require_relative 'test_helper'
 
-describe "" do
+describe "#main" do
 	
-	it "" do
-		
-
-	end
+	# it "runs the program" do
+	# 	VCR.use_cassette("slack_create") do
+	# 		expect(workspace.channels).must_be_instance_of SlackCLI::Workspace
+	# 	end
+	# end
 
 end
