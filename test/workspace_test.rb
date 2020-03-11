f6cd87fa32a4892261eefbf352e98cd0492180ad
Jessica Liang <jessicursliang@gmail.com>
2020-03-10 21:37:03 -0700

    updated testing for Workspace

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -1,10 +1,17 @@
 require_relative 'test_helper'
 
-describe "" do
+describe "initialize" do
 	
-	it "" do
-		
+	before do
+		VCR.use_cassette("workspace_create") do
+			@workspace = SlackCLI::Workspace.new
+		end
+	end
 
+	it "creates an instance of Workspace" do
+		expect(@workspace).must_be_instance_of SlackCLI::Workspace
+		expect(@workspace.users).must_be_instance_of Array
+		expect(@workspace.channels).must_be_instance_of Array
 	end
 
 end
