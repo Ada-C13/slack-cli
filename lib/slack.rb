0667d426fc7ba109fa58e35e598dbab87f80ad43
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-11 14:18:21 -0700

    Created a channel array

--- lib/slack.rb
+++ lib/slack.rb
@@ -4,20 +4,36 @@ require "dotenv"
 require_relative "workspace"
 Dotenv.load
 
-# def test
-# url = "https://slack.com/api/channels.list?token=#{SLACK_TOKEN}&pretty=1"
-# response = HTTParty.get(url)
-# p response
-# end
-#  p test
+SLACK_TOKEN = ENV["SLACK_TOKEN"]
 
-def main
-  puts "Welcome to the Ada Slack CLI!"
-  workspace = Workspace.new
+def test
+url = "https://slack.com/api/channels.list?token=#{SLACK_TOKEN}&pretty=1"
+response = HTTParty.get(url)
 
-  # TODO project
-
-  puts "Thank you for using the Ada Slack CLI"
+ 
+channel_list = []
+response["channels"].each do |channel|
+channel_list << channel["name"]
 end
 
-main if __FILE__ == $PROGRAM_NAME
+return channel_list
+
+end
+ p test
+
+
+ # {
+#   "Our_channels" => {
+#     :name => respnse[0]["channels"]["name"]
+#   }
+# }
+# def main
+#   puts "Welcome to the Ada Slack CLI!"
+#   workspace = Workspace.new
+
+#   # TODO project
+
+#   puts "Thank you for using the Ada Slack CLI"
+# end
+
+# main if __FILE__ == $PROGRAM_NAME
