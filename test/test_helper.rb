39bceea2d1c315d81b3eca0c0ffef714908e9351
Nikki Vaughan <vnikki13@gmail.com>
2020-03-10 17:01:04 -0700

    required relative all lib files for project

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,12 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
 
+require_relative "../lib/slack"
+require_relative "../lib/workspace"
+require_relative "../lib/channel"
+require_relative "../lib/user"
+require_relative "../lib/recipient"
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
@@ -25,5 +31,7 @@ VCR.configure do |config|
   }
 
   # Don't leave our token lying around in a cassette file.
-
+  config.filter_sensitive_data("SLACK_TOKEN") do
+    ENV["SLACK_TOKEN"]
+  end
 end
