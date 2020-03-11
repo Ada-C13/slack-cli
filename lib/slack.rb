7c83b4f6f996778f42ad9cf214df9ed3b46420a5
Jessica Stone <jstone4815@gmail.com>
2020-03-11 14:59:31 -0700

    set up connection to API

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,10 +1,21 @@
 #!/usr/bin/env ruby
+require 'dotenv'
+require 'httparty'
+require_relative 'workspace'
+
+Dotenv.load
+BASE_URL = "https://slack.com/api/conversations.list"
+SLACK_TOKEN = ENV["SLACK_TOKEN"]
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
-  # TODO project
+  response = HTTParty.get(BASE_URL, query: {token: SLACK_TOKEN})
+
+  # response["channels"].each do |channel|
+  #   puts channel["name"]
+  # end
 
   puts "Thank you for using the Ada Slack CLI"
 end
