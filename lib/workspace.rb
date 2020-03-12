fead976cc01561eac5aae8af9a42243068484b1c
cojenco <icathyouyang@gmail.com>
2020-03-11 22:31:08 -0700

    Revised methods to take out parameters and instead prompt user gets.chomp within. Added helper method #find_recipient and tests.

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -14,42 +14,102 @@ class Workspace
     @selected = nil 
   end
 
-  def list_users
-    raise SlackAPIError, "No users were loaded from the API." if @users.length == 0
-    tp @users, :slack_id, :name, :real_name
+  def find_recipient(id_name)
+    channel_found = @channels.find{|channel|channel.slack_id == id_name || channel.name == id_name }
+    user_found = @users.find{|channel|channel.slack_id == id_name || channel.name == id_name}
+    found_recipient = nil
+
+    if channel_found != nil
+      found_recipient = channel_found
+    elsif user_found != nil
+      found_recipient = user_found
+    else
+      raise ArgumentError, 'A valid SLACK ID or username is required'
+    end
+
+    return found_recipient
+  end
+
+  def select_user
+    puts "To select user, please enter a Slack ID or username:"
+    user_input = gets.chomp
+    @selected = find_recipient(user_input)
+  end
+
+  def select_channel
+    puts "To select channel, please enter a Slack ID or name:"
+    user_input = gets.chomp
+    @selected = find_recipient(user_input)
+  end
+
+  def send_message
+    raise ArgumentError, "There is not yet a selected recipient" if @selected == nil
+    puts "Enter the message you want to send out:"
+    user_input = gets.chomp
+    @selected.send_message(user_input)
     return true
   end
 
-  def list_channels
-    raise SlackAPIError, "No channels were loaded from the API." if @channels.length == 0
-    tp @channels, :slack_id, :name, :topic, :member_count
+  def show_details
+    @selected.get_details
     return true
   end
+end
 
-  def select_user(id: nil, username: nil)
-    if id
-      @selected = @users.find{|user|user.slack_id == id}
-    elsif username
-      @selected = @users.find{|user|user.name == username}
-    else
-      raise ArgumentError, 'SLACK ID or username is required'
-    end
 
-    return @selected
-  end
+# def select_user(id_name)
+#   found_by_id = @users.find{|user|user.slack_id == id_name}
+#   found_by_name = @users.find{|user|user.name == id_name}
 
-  def select_channel(id: nil, name: nil)
-    if id
-      @selected = @channels.find{|channel|channel.slack_id == id}
-    elsif name
-      @selected = @channels.find{|channel|channel.name == name}
-    else
-      raise ArgumentError, 'SLACK ID or channel name is required'
-    end
+#   if found_by_id != nil
+#     @selected = found_by_id
+#   elsif found_by_name != nil
+#     @selected = found_by_name
+#   else
+#     raise ArgumentError, 'A valid SLACK ID or username is required'
+#   end
 
-    return @selected
-  end
+#   return @selected
+# end
 
+# #methods without parameters, I could prompt user gets.chomp inside the method. But is that right? How to test?
+
+# def select_channel(id_name)
+#   found_by_id = @channels.find{|channel|channel.slack_id == id_name}
+#   found_by_name = @channels.find{|channel|channel.name == id_name}
+
+#   if found_by_id != nil
+#     @selected = found_by_id
+#   elsif found_by_name != nil
+#     @selected = found_by_name
+#   else
+#     raise ArgumentError, 'A valid SLACK ID or username is required'
+#   end
+
+#   return @selected
+# end
+
+  # def list_users
+  #   tp @users, :slack_id, :name, :real_name
+  #   return true
+  # end
+
+  # def list_channels
+  #   tp @channels, :slack_id, :name, :topic, :member_count
+  #   return true
+  # end
+
+  # def select_user(id: nil, username: nil)
+  #   if id
+  #     @selected = @users.find{|user|user.slack_id == id}
+  #   elsif username
+  #     @selected = @users.find{|user|user.name == username}
+  #   else
+  #     raise ArgumentError, 'SLACK ID or username is required'
+  #   end
+
+  #   return @selected
+  # end
 
   # def send_message
   #   if @selected is a channel
@@ -58,4 +118,6 @@ class Workspace
   #     @selected.send_message #but channel#send_message needs argument??
   #   end
   # end
-end
+
+# testspace = Workspace.new
+# testspace.send_message("what what")
--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -34,72 +34,99 @@ describe "workspace" do
     end
   end
 
-  describe "select_user" do
-    it "returns the instance variable @selected" do
+  describe "find_recipient" do
+    it "returns the recipient found" do
       VCR.use_cassette("workspace_endpoint") do
         test_space = Workspace.new
-        expect(test_space.select_user(id: "bogus")).must_be_nil
-        expect(test_space.select_user(id: "USLACKBOT")).must_equal test_space.selected
-        expect(test_space.select_user(username: "slackbot")).must_equal test_space.selected
+        expect(test_space.find_recipient("USLACKBOT")).must_be_instance_of User
+        expect(test_space.find_recipient("slackbot")).must_be_instance_of User
+        expect(test_space.find_recipient("general")).must_be_instance_of Channel
+        expect(test_space.find_recipient("random")).must_be_instance_of Channel
       end
     end
 
-    it "raises an ArgumentError if neither the Slack ID or name is passed in" do
+    it "raises an ArgumentError if no or invalid Slack ID or name is passed in" do
       VCR.use_cassette("workspace_endpoint") do
         test_space = Workspace.new
-        expect{test_space.select_user()}.must_raise ArgumentError
-      end
-    end
-  end
-
-  describe "select_channel" do
-    it "returns the instance variable @selected" do
-      VCR.use_cassette("workspace_endpoint") do
-        test_space = Workspace.new
-        expect(test_space.select_channel(id: "bogus")).must_be_nil
-        expect(test_space.select_channel(name: "general")).must_equal test_space.selected
-      end
-    end
-
-    it "raises an ArgumentError if neither the Slack ID or name is passed in" do
-      VCR.use_cassette("workspace_endpoint") do
-        test_space = Workspace.new
-        expect{test_space.select_channel()}.must_raise ArgumentError
+        expect{test_space.find_recipient()}.must_raise ArgumentError
+        expect{test_space.find_recipient("bogusbogustestbogus")}.must_raise ArgumentError
       end
     end
   end
 
-  describe "#list_users" do
-    it "can list all the users from @users" do
+  describe "#send_message" do
+    it "will raise an ArgumentError when there is no selected recipient" do
       VCR.use_cassette("workspace_endpoint") do
         test_space = Workspace.new
-        response = test_space.list_users
+        expect{test_space.send_message}.must_raise ArgumentError
+      end
+    end
+  end 
+
+  describe "#show_details" do
+    it "can show the details of the user" do
+      VCR.use_cassette("workspace_endpoint") do
+        test_space = Workspace.new
+        user01 = test_space.users[0]
+        test_space.selected = user01
+        response = test_space.show_details
         expect(response).must_equal true
       end
     end
-
-    # it "will raise an exception when the recipient name or id is invalid" do
-    #   VCR.use_cassette("workspace_endpoint") do
-    #     test_space = Workspace.new
-    #     expect {test_space.list_users}.must_raise SlackAPIError
-    #   end
-    # end
-  end
-
-  describe "#list_channels" do
-    it "can list all the channels from @channels" do
-      VCR.use_cassette("workspace_endpoint") do
-        test_space = Workspace.new
-        response = test_space.list_channels
-        expect(response).must_equal true
-      end
-    end
-
-    # it "will raise an exception when the recipient name or id is invalid" do
-    #   VCR.use_cassette("workspace_endpoint") do
-    #     test_space = Workspace.new
-    #     expect {test_space.list_channels}.must_raise SlackAPIError
-    #   end
-    # end
   end
 end
+  # describe "select_user" do
+  #   it "returns the instance variable @selected" do
+  #     VCR.use_cassette("workspace_endpoint") do
+  #       test_space = Workspace.new
+  #       expect(test_space.select_user("USLACKBOT")).must_equal test_space.selected
+  #       expect(test_space.select_user("slackbot")).must_equal test_space.selected
+  #     end
+  #   end
+
+  #   it "raises an ArgumentError if no or invalid Slack ID or name is passed in" do
+  #     VCR.use_cassette("workspace_endpoint") do
+  #       test_space = Workspace.new
+  #       expect{test_space.select_user()}.must_raise ArgumentError
+  #       expect{test_space.select_user("bogus")}.must_raise ArgumentError
+  #     end
+  #   end
+  # end
+
+  # describe "select_channel" do
+  #   it "returns the instance variable @selected" do
+  #     VCR.use_cassette("workspace_endpoint") do
+  #       test_space = Workspace.new
+  #       expect(test_space.select_channel("general")).must_equal test_space.selected
+  #       expect(test_space.select_channel("random")).must_equal test_space.selected
+  #     end
+  #   end
+
+  #   it "raises an ArgumentError if no or invalid Slack ID or name is passed in" do
+  #     VCR.use_cassette("workspace_endpoint") do
+  #       test_space = Workspace.new
+  #       expect{test_space.select_channel()}.must_raise ArgumentError
+  #       expect{test_space.select_channel("thisbogusidshouldraiseerror")}.must_raise ArgumentError
+  #     end
+  #   end
+  # end
+
+  # describe "#list_users" do
+  #   it "can list all the users from @users" do
+  #     VCR.use_cassette("workspace_endpoint") do
+  #       test_space = Workspace.new
+  #       response = test_space.list_users
+  #       expect(response).must_equal true
+  #     end
+  #   end
+  # end
+
+  # describe "#list_channels" do
+  #   it "can list all the channels from @channels" do
+  #     VCR.use_cassette("workspace_endpoint") do
+  #       test_space = Workspace.new
+  #       response = test_space.list_channels
+  #       expect(response).must_equal true
+  #     end
+  #   end
+  # end
