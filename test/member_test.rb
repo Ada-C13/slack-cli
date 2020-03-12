686bcba8f7a081112043332fc5dc92e4c7e31634
Jocelyn Wang <wangjoc@gmail.com>
2020-03-12 12:16:04 -0700

    added in test for users

--- test/member_test.rb
+++ test/member_test.rb
@@ -1,52 +1,20 @@
 require_relative 'test_helper'
 
-describe "Member - Working Case" do
-  # before do
-  #   VCR.use_cassette("working_case") do
-  #     @workspace = Workspace.new
-  #   end
-  # end
-
-  # it 'list out all users' do
-  #   expect(@workspace.users.first).must_be_kind_of Member
-  #   expect(@workspace.users.first.name).must_include 'slackbot'
-  #   expect(@workspace.users.length).must_equal 3 
-  # end
-
-  # it 'list out all users' do
-  #   expect(@workspace.channels.first).must_be_kind_of Channel
-  #   expect(@workspace.channels.first.name).must_include 'general'
-  #   expect(@workspace.channels.length).must_equal 3 
-  # end
-end
-
-describe "Slack API - Non-working Case" do
-  it "will raise an excption if no token provided" do
-
+describe "User Access - Working Case" do
+  before do
+    VCR.use_cassette("list_users") do
+      @users = Member.all
+    end
   end
 
-  # before do
-  #   VCR.use_cassette("working_case") do
-  #     @workspace = Workspace.new
-  #   end
-  # end
+  it 'inherits name and id from recipient' do
+    expect(@users.first.name).must_equal'slackbot'
+    expect(@users.first.id).must_equal 'USLACKBOT' 
+  end
 
-  # it 'list out all users' do
-  #   expect(@workspace.users.first).must_be_kind_of Member
-  #   expect(@workspace.users.first.name).must_include 'slackbot'
-  #   expect(@workspace.users.length).must_equal 3 
-  # end
+  it 'list out all users' do
+    expect(@users.first).must_be_kind_of Member
+    expect(@users.length).must_equal 3 
+  end
 
-  # it 'list out all users' do
-  #   expect(@workspace.channels.first).must_be_kind_of Channel
-  #   expect(@workspace.channels.first.name).must_include 'general'
-  #   expect(@workspace.channels.length).must_equal 3 
-  # end
 end
-
-
-  # user can exit out of program
-  # after command "quit" is entered, unable to interact with interface anymore
-
-
-  # Messages are sent from bot
