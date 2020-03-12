93db72f9dc40e6c464df2b074515fd4ebe2764fe
Catherina <catherine.pchelina@gmail.com>
2020-03-11 20:23:07 -0700

    Add ask_for_action and perfom action methods

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,10 +1,42 @@
 #!/usr/bin/env ruby
+require "awesome_print"
+require 'table_print'
+require_relative "workspace"
+
+
+def ask_for_action
+  puts "* Select from the options below:"
+  puts "* list users"
+  puts "* list channels"
+  puts "* quit" 
+  puts
+end
+
+def perform_action(action, workspace)
+  case action
+  when "list users"
+    workspace.list_users
+  when "list channels"
+    workspace.list_channels
+  end
+end
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
-  # TODO project
+  puts "Number of users loaded = #{workspace.users.length}" 
+  puts "Number of channels loaded = #{workspace.channels.length}"
+  puts
+  ask_for_action
+  action = gets.chomp.downcase
+  until action == "quit"
+    puts perform_action(action, workspace)
+    puts
+    ask_for_action
+    action = gets.chomp.downcase
+  end
+  
 
   puts "Thank you for using the Ada Slack CLI"
 end
