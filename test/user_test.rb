16a15a78b4ec1c56ee0bb1878ac2523d0612672e
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 15:58:58 -0700

    tests for list_all users and the details method

--- test/user_test.rb
+++ test/user_test.rb
@@ -2,7 +2,29 @@ require_relative "test_helper"
 # Any tests involving a User should use the username 'SlackBot'
 
 describe 'User class' do
-  it 'should make a new instance of User' do
+  describe "self.list_all" do
+    it 'should list all users' do
+      VCR.use_cassette("users") do
+        response = Recipient.get("https://slack.com/api/users.list")
+        all_users = User.list_all
+        expect(response.code).must_equal 200
+        expect(all_users[0].name).must_equal "slackbot"
+        expect(all_users[0].slack_id).must_equal "USLACKBOT"
+        expect(all_users[0].real_name).must_equal "Slackbot"
+      end
+    end
+  end
 
+  describe "details" do
+    it 'should get the details of a specified user' do
+      VCR.use_cassette("users") do
+        response = Recipient.get("https://slack.com/api/users.list")
+        slackbot = User.new(slack_id: "USLACKBOT", name: "slackbot", real_name:"Slackbot")
+        expect(response.code).must_equal 200
+        expect(slackbot.details).must_include slackbot.name
+        expect(slackbot.details).must_include slackbot.real_name
+        expect(slackbot.details).must_include slackbot.slack_id
+      end
+    end
   end
 end
