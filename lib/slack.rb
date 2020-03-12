418394b9764b6178ddc3ff6cb73d1e661b289dd0
Kate Mangubat <kate.d.mangubat@gmail.com>
2020-03-11 23:44:46 -0700

    added  CLI for list users

--- lib/slack.rb
+++ lib/slack.rb
@@ -4,14 +4,28 @@ require 'dotenv'
 require 'table_print'
 require_relative 'workspace'
 
-
 def main
-  puts "Welcome to the Ada Slack CLI!"
   workspace = SlackCli::Workspace.new
-
+  puts "Welcome to the Ada Slack CLI!"
+  puts "Here are the options:"
+  options = ["list users", "list channels", "quit"]
+  options.each do |option|
+    puts option
+  end
+  
+  user_input = gets.chomp.downcase
+  while user_input != options[-1]
+    if user_input == options[0]
+      #list users with username, real name, and Slack ID.
+      tp workspace.users, :id, :name, :real_name #https://github.com/arches/table_print usage
+    end 
+    user_input = gets.chomp
+  end
+  
+  
   # TODO project
 
   puts "Thank you for using the Ada Slack CLI"
-end
+end #main
 
 main if __FILE__ == $PROGRAM_NAME
