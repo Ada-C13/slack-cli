2985810baf301ba11f2a2b6e3a837dff91f3219b
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 15:09:51 -0700

    Two tests describing the result from getting users and channels info

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -1,21 +1,30 @@
 require_relative "test_helper"
 
 describe 'Recipient class' do
-  describe 'attributes' do
-    it 'should make a new instance of Recipient' do
-
-    end
-  end
-
-  describe 'get method can get information from a provided url and params' do
+  describe 'get method can get information from a provided url' do
     it 'gets channels.list' do
       VCR.use_cassette("channels") do
         params = {
           token: ENV["SLACK_TOKEN"]
         }
-        response = Recipient.get("https://slack.com/api/channels.list", params)
+        response = Recipient.get("https://slack.com/api/channels.list")
 
-        expect(response.code).must_equal 200
+        expect(response["ok"]).must_equal true
+        expect(response["channels"].length).must_equal 3
+        expect(response["channels"][0]).must_include "id"
+      end
+    end
+
+    it 'gets users.list' do
+      VCR.use_cassette("users") do
+        params = {
+          token: ENV["SLACK_TOKEN"]
+        }
+        response = Recipient.get("https://slack.com/api/users.list")
+        p response
+        expect(response["ok"]).must_equal true
+        expect(response["members"].length).must_equal 10
+        expect(response["members"][0]).must_include "id"
       end
     end
   end
