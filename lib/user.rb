21164d106f66b0386548264ba558c766d636822a
Kate Mangubat <kate.d.mangubat@gmail.com>
2020-03-11 23:15:15 -0700

    added User class and load_all method

---
+++ lib/user.rb
@@ -0,0 +1,64 @@
+require 'httparty'
+require 'dotenv'
+Dotenv.load(__dir__ + "/" + "../.env") #https://github.com/bkeepers/dotenv
+require_relative 'recipient'
+
+
+module SlackCli
+  class User < Recipient
+    attr_accessor :real_name, :status_text, :status_text, :status_emoji
+
+    def initialize(id, name, real_name, status_text = nil, status_emoji = nil)
+      super(id, name)
+      @real_name = real_name
+      @status_text = status_text
+      @status_emoji = status_emoji
+    end #initialize
+
+    #retrieves list of users from Slack
+    #input
+    #returns list of users [User]
+    def self.load_all()
+      users = []
+      #send request to Slack API using users.list endpoint
+      #                               after?  param =    value
+      url = "https://slack.com/api/users.list?token=#{ENV['SLACK_TOKEN']}" #https://github.com/bkeepers/dotenv
+      response = HTTParty.get(url) #request that will return the response
+      #parse response and get users
+        response['members'].each do |member|
+          # "id": "UUUKJ03NX",
+          # "team_id": "TV63QKAAU",
+          # "name": "kate.d.mangubat",
+          # "deleted": false,
+          # "color": "4bbe2e",
+          # "real_name": "Kate Mangubat"
+  
+          #create a user
+          id = member["id"]
+          name = member["name"]
+          real_name = member["real_name"]
+          status_text = member["profile"]["status_text"]
+          status_emoji = member["profile"]["status_emoji"]
+          #handle
+          #real name
+          slack_user = SlackCli::User.new(id, name, real_name, status_text, status_emoji)
+          #save to users
+          users.push(slack_user)
+        end
+      return users
+    end #load_all method
+
+  end #class
+end #module
+
+# testuser = SlackCli::User.new(1, "kate", "katem")
+# puts testuser.handle
+all_users = SlackCli::User.load_all
+
+all_users.each do |user|
+  puts user.name
+  puts user.real_name
+  puts user.status_text
+  puts user.status_emoji
+end
+
