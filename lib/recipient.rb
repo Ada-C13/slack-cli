8b73dcb18bd235463933fb68eab66669460b35b5
Maryam Heshmati <mairheshmati@gmail.com>
2020-03-12 15:30:09 -0700

    implemented method using HTTParty to post message on slack

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -11,18 +11,31 @@ class Recipient
     @slack_id = slack_id
   end
 
+
+  def send_message(channel, message)
+    url = "https://slack.com/api/chat.postMessage"
+    query = {
+      token: AUTH_TOKEN,
+      channel: channel,
+      text: message,
+    }
+    response = HTTParty.post(url, query: query)
+    raise(Exception, "Failed to send your message") if response["true"] == false 
+  end
+
   private
-#???????????????
+
   def self.get(base_url)
     query = {
       token: AUTH_TOKEN,
     }
     response = HTTParty.get(base_url, query: query)
-
-    return response unless response["okay"] == false
+    raise(Exception, "Failed to access API, check url") if response["okay"] == false
+    return response
   end
 
   def self.list_all
     raise(NotImplementedError, "Implement me in a child class!")
   end
+
 end
