e1807673ba1f8f2eeaf6cc65c4f344dc89ff219e
Haben <haben7@gmail.com>
2020-03-12 15:23:22 -0700

    added requires needed & dotenv.load

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,12 +1,25 @@
 #!/usr/bin/env ruby
+require 'dotenv'
+require 'httparty'
+require 'awesome_print'
+require_relative 'workspace'
+
+# TODO project
+Dotenv.load
+
+
+
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
-  # TODO project
+
+  
+
+
 
   puts "Thank you for using the Ada Slack CLI"
 end
 
-main if __FILE__ == $PROGRAM_NAME
+ main if __FILE__ == $PROGRAM_NAME
