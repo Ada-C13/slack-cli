07a953d117d8fa2b5412cb3ce2ed11be1f9e0f9f
Yieni <yaynee8d@gmail.com>
2020-03-12 15:55:36 -0700

    set up token and hide token. test that token works

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,12 +1,26 @@
 #!/usr/bin/env ruby
+require 'httparty'
+require 'dotenv'
+Dotenv.load
 
-def main
-  puts "Welcome to the Ada Slack CLI!"
-  workspace = Workspace.new
+# def main
+#   puts "Welcome to the Ada Slack CLI!"
+#   workspace = Workspace.new
 
-  # TODO project
+#   # TODO project
+  
 
-  puts "Thank you for using the Ada Slack CLI"
-end
+#   puts "Thank you for using the Ada Slack CLI"
+# end
 
-main if __FILE__ == $PROGRAM_NAME
+# main if __FILE__ == $PROGRAM_NAME
+
+
+BASE_URL = "https://slack.com/api/channels.list"
+SLACK_TOKEN = ENV["SLACK_TOKEN"]
+
+params = {token: SLACK_TOKEN}
+response = HTTParty.get(BASE_URL, query: params)
+p SLACK_TOKEN
+p params
+p response
