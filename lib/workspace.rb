249311c684cf4fba1d5ab921371b7d819e23c8c3
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 16:23:40 -0700

    makes new instances of a User and a Channel

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,8 +1,11 @@
+require_relative 'user'
+require_relative 'channel'
+
 class Workspace
   attr_reader :users, :channels
   
   def initialize
-    @users = []
-    @channels = []
+    @users = User.list_all
+    @channels = Channel.list_all
   end
 end
