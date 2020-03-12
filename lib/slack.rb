dba4819499a42e12a590aad5d6bd9ae34d572655
cojenco <icathyouyang@gmail.com>
2020-03-11 22:29:23 -0700

    Revised driver code file. Moved multiple prompts into Workspace.

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,19 +1,29 @@
 #!/usr/bin/env ruby
 require_relative 'workspace'
-# require 'httparty'
-# require 'dotenv'
 
-# # Tell dotenv to look for the .env file
-# Dotenv.load
 
 def display_options
   puts "\nEnter one of the following options."
   puts "1. list users"
   puts "2. list channels"
+  puts "3. select user"
+  puts "4. select channel"
+  puts "5. details"
+  puts "6. send message"
   puts "0. quit"
 end
 
+def get_id_name(recipient)
+  puts "To select a #{recipient}, please enter a Slack ID or name:"
+  user_input = gets.chomp
+  return user_input
+end
 
+def prompt_message
+  puts "Enter the message you want to send out:"
+  user_input = gets.chomp
+  return user_input
+end
 
 def main
   puts "Welcome to the Ada Slack CLI!"
@@ -27,25 +37,49 @@ def main
     choice = gets.chomp
     case choice
       when "1", "list users"
-        workspace.list_users
+        tp workspace.users, :slack_id, :name, :real_name
       when "2", "list channels"
-        workspace.list_channels
+        tp workspace.channels, :slack_id, :name, :topic, :member_count
+      when "3", "select user"
+        # user_input = get_id_name("user")
+        begin
+          chosen = workspace.select_user
+        rescue ArgumentError => exception
+          puts "Encountered an error: #{exception}"
+        end
+      when "4", "select channel"
+        # user_input = get_id_name("channel")
+        begin
+          chosen = workspace.select_channel
+        rescue ArgumentError => exception
+          puts "Encountered an error: #{exception}"
+        end
+      when "5", "details"
+        if workspace.selected != nil
+          begin
+            workspace.show_details
+          rescue StandardError => exception
+            puts "Encountered an error: #{exception}"
+          end
+        else
+          puts "There is no recipient selected. Please select a user or channel first."
+        end
+      when "6", "send message"
+        if workspace.selected != nil
+          begin
+            workspace.send_message
+          rescue StandardError => exception
+            puts "Encountered an error: #{exception}"
+          end
+        else
+          puts "There is no recipient selected. Please select a user or channel first."
+        end
       when "0", "quit", "exit"
         control_loop = false
     end
   end
 
-  
   puts "Thank you for using the Ada Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
-
-
-# USERS_URI = "https://slack.com/api/users.list"
-# CHANNELS_URI = "https://slack.com/api/conversations.list"
-
-# SLACK_TOKEN = ENV["SLACK_TOKEN"]
-
-# response = HTTParty.get(USERS_URI, query: {token: SLACK_TOKEN})
-# puts response
