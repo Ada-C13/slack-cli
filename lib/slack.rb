2c9ab14698da933992d686bfbf03278f4cc8db7b
dnguye2 <dnguyen1093@gmail.com>
2020-03-12 14:57:02 -0700

    slack cli menu input created

--- lib/slack.rb
+++ lib/slack.rb
@@ -2,30 +2,66 @@
 
 require 'httparty'
 require 'dotenv'
+require 'table_print'
+require_relative 'workspace'
 
 Dotenv.load
 
-URL = "https://slack.com/api/channels.list"
+def get_user_choice
+  puts "Welcome to the Ada Slack CLI!"
+  print "Please select one of the options: 
+  (1) list users
+  (2) list channels
+  (3) quit
+  \n Selection: "
 
-query_parameters = {
-  token: ENV['SLACK_API_TOKEN']
-}
 
-response = HTTParty.get(URL, query: query_parameters)
+  action = gets.chomp
 
-response["channels"].each do |channel|
-  puts channel["name"]
+  puts ""
+
+  return action
+end
+
+def main
+  # main code layout inspired by instructor solar system implementation
+  program_running = true
+
+  while (program_running)
+
+    user_choice = get_user_choice
+
+    case user_choice.downcase
+    when "list users"
+      puts "list users not implemented yet"
+      #TODO list all users in Slack workspace
+      # has username, real name, and Slack ID
+    
+    when "list channels"
+      #TODO list all channels
+      # include channel name, topic, member count, and Slack ID
+      url = "https://slack.com/api/channels.list"
+      workspace = Workspace.new
+    
+      query_parameters = {
+        token: ENV['SLACK_API_TOKEN']
+      }
+
+      response = HTTParty.get(url, query: query_parameters)
+
+      response["channels"].each do |channel|
+        puts channel["name"]
+      end
+    
+    when "quit"
+      program_running = false
+      puts "Thank you for using the Ada Slack CLI"
+    else
+      puts "Invalid selection. Please provide a valid action."
+    end
+  end
 end
 
 
 
-# def main
-#   puts "Welcome to the Ada Slack CLI!"
-#   workspace = Workspace.new
-
-#   # TODO project
-
-#   puts "Thank you for using the Ada Slack CLI"
-# end
-
-# main if __FILE__ == $PROGRAM_NAME
+main if __FILE__ == $PROGRAM_NAME
