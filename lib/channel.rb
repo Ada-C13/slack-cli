bc2d02641ce85894783e12f09257424178d27ce0
Jessica Stone <jstone4815@gmail.com>
2020-03-12 16:07:29 -0700

    add self.list_all method to Channel and User

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,5 +1,9 @@
+require_relative "recipient"
+
 class Channel < Recipient
   attr_reader :topic, :member_count
+
+  @all_channels = []
 
   def initialize
     @topic = topic
@@ -12,6 +16,19 @@ class Channel < Recipient
     # When I type list channels, I should see a list of all the channels for that workspace. This list should include the channel's name, topic, member count, and Slack ID.
 
   def self.list_all
+    response = HTTParty.get(BASE_URL + "conversations.list", query: QUERY)
+    all_channels = []
+
+    response["channels"].each do |channel|
+      all_channels << {
+        name: channel["name"],
+        topic: channel["topic"]["value"],
+        member_count: channel["num_members"],
+        id: channel["id"]
+      } 
+    end
+    
+    return all_channels
   end
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,9 @@
+require_relative 'recipient'
+
 class User < Recipient
-  attr_reader :real_name, :status_text, :status_emoji
+  attr_reader :real_name, :status_text, :status_emoji, :all_users
 
-  BASE_URL = "https://slack.com/api/users.list"
-  API_KEY = ENV["SLACK_TOKEN"]
-
-  def initialize(slack_id)
+  def initialize(slack_id, name)
     super(slack_id, name)
     @real_name = real_name
     @status_text = status_text
@@ -13,21 +12,33 @@ class User < Recipient
 
   def details
     # This list should include username, real name, and Slack ID.
-    response = HTTParty.get(BASE_URL, query: {token: API_KEY})
-    member = {}
+    # response = HTTParty.get(BASE_URL, query: {token: API_KEY})
+    # member = {}
 
-    response["members"].each do |member|
-      if member["id"] == @slack_id
-        @real_name = member["real_name"]
-        @name = member["name"]
-        @status_text = member["status_text"]
-        @status_emoji = member["status_emoji"]
-      end
-    end
+    # response["members"].each do |member|
+    #   if member["id"] == @slack_id
+    #     @real_name = member["real_name"]
+    #     @name = member["name"]
+    #     @status_text = member["status_text"]
+    #     @status_emoji = member["status_emoji"]
+    #   end
+    # end
   end
 
   # When I type list users, I should see a list of all the users in the Slack workspace
   def self.list_all
+    response = HTTParty.get(BASE_URL + "users.list", query: QUERY)
+    all_users = []
+
+    response["members"].each do |member|
+      all_users << {
+        name: member["name"],
+        real_name: member["real_name"],
+        id: member["id"]
+      } 
+    end
+    
+    return all_users
   end
 
 end
