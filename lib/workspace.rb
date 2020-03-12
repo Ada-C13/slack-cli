be85ac159579d55b5b0dc07e5d98bd5d68bf8d46
Catherina <catherine.pchelina@gmail.com>
2020-03-11 20:24:39 -0700

    Add list_users and list_channels methods

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -9,4 +9,12 @@ class Workspace
     @channels = Channel.list_all
   end
 
+  def list_users
+    @users.map { |user| "slack_id: #{user.slack_id}, user: #{user.username}, username: #{user.name}"}
+  end
+
+  def list_channels
+    @channels.map { |channel| "slack_id: #{channel.slack_id}, channel: #{channel.name}, topic: #{channel.topic}, member count: #{channel.member_count}"}
+  end
+
 end
