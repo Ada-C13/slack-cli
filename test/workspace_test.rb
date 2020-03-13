9851c3d3318e67badd2b618952e43ae61fe4797b
quinqu <quinruby0@gmail.com>
2020-03-13 16:28:58 -0700

    implemented some tests in workspace_test.rb

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -1,20 +1,19 @@
 require_relative 'test_helper'
 require_relative '../lib/workspace'
+require_relative '../lib/channel'
+require_relative '../lib/user'
+
 
 describe "initialize" do 
 
   it "will create a workspace object" do 
     workspace = Workspace.new
     workspace.must_be_instance_of Workspace
-  end 
 
-  # it "can return all users" do 
-  #   VCR.use_cassette("user_data") do
-  #     workspace = Workspace.new
-  #     users = workspace.list_users
-  #     users.wont_be_nil
-  #   end
-  # end 
+    
+
+
+  end 
 
 
 
