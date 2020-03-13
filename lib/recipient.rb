17f223dc1bf5c711744b17e10c8b16e7f097103d
Katie <katie.vandervoort@gmail.com>
2020-03-12 20:44:36 -0700

    implemented send_message method

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,4 +1,11 @@
 require 'HTTParty'
+require 'dotenv'
+
+# require_relative 'channel.rb'
+# require_relative 'user.rb'
+
+Dotenv.load
+
 
 class Recipient
   attr_reader :slack_id, :name
@@ -8,6 +15,18 @@ class Recipient
   end
 
   def send_message(message)
+    url = "https://slack.com/api/chat.postMessage"
+
+    response = HTTParty.post(
+      url,
+      body:  {
+        token: ENV['SLACK_TOKEN'],
+        text: message,
+        channel: @slack_id
+      },
+      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    )
+
   end
 
   def self.get(url,params)
