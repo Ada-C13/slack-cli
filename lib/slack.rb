96cd895df6aa546660ecdd0bdf1717d6c61f98b6
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 18:42:56 -0700

    Added Wave 2 options for user input

--- lib/slack.rb
+++ lib/slack.rb
@@ -17,24 +17,42 @@ end
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
-  
-  puts "Choose an option
-  - list users
-  - list channels
-  - quit"
-  print "=> "
-  user_option = gets.chomp
 
-  case user_option
-  when "list users"
-    list_users(workspace)
-  when "list channels"
-    list_channels(workspace)
-  when "quit"
-    exit
+  user_option = ""
+
+  until user_option == "quit"
+    puts "Choose an option
+    - list users
+    - list channels
+    - select user
+    - select channel
+    - details
+    - quit"
+    print "=> "
+    user_option = gets.chomp
+
+    case user_option
+    when "list users"
+      list_users(workspace)
+    when "list channels"
+      list_channels(workspace)
+    when "select user"
+      puts "Input user ID or Name"
+      print "=> "
+      workspace.select_user(gets.chomp)
+    when "select channel"
+      puts "Input channel ID or Name"
+      print "=> "
+      workspace.select_channel(gets.chomp)
+    when "details"
+      if workspace.selected.nil?
+        puts "Sorry, you have nothing selected"
+      else
+        puts "Here are your for #{workspace.selected}"
+        workspace.selected.details
+      end
+    end
   end
-
-  main
 
   puts "Thank you for using the Ada Slack CLI"
 end
