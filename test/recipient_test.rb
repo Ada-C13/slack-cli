9d0fd54d0353569de8296772bcc02b045d300fd0
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 17:22:31 -0700

    Don't need params

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -4,9 +4,6 @@ describe 'Recipient class' do
   describe 'get method can get information from a provided url' do
     it 'gets channels.list' do
       VCR.use_cassette("channels") do
-        params = {
-          token: ENV["SLACK_TOKEN"]
-        }
         response = Recipient.get("https://slack.com/api/channels.list")
 
         expect(response["ok"]).must_equal true
@@ -17,11 +14,8 @@ describe 'Recipient class' do
 
     it 'gets users.list' do
       VCR.use_cassette("users") do
-        params = {
-          token: ENV["SLACK_TOKEN"]
-        }
         response = Recipient.get("https://slack.com/api/users.list")
-        p response
+
         expect(response["ok"]).must_equal true
         expect(response["members"].length).must_equal 10
         expect(response["members"][0]).must_include "id"
