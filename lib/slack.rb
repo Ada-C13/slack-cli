d401c7ef0332228b5f12230a266c4dfa7f9c047f
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 17:26:04 -0700

    Files are all passing for Wave 1 tests

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,40 @@
 #!/usr/bin/env ruby
 require_relative 'workspace'
 
+def list_users(workspace)
+  workspace.users.each do |user|
+    user.details
+  end
+end
+
+def list_channels(workspace)
+  workspace.channels.each do |channel|
+    channel.details
+  end
+end
+
+
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
+  
+  puts "Choose an option
+  - list users
+  - list channels
+  - quit"
+  print "=> "
+  user_option = gets.chomp
 
-  # TODO project
+  case user_option
+  when "list users"
+    list_users(workspace)
+  when "list channels"
+    list_channels(workspace)
+  when "quit"
+    exit
+  end
+
+  main
 
   puts "Thank you for using the Ada Slack CLI"
 end
