4b7307f8cb8a6c0a7d2bb1009a68dc099b39725d
dnguye2 <dnguyen1093@gmail.com>
2020-03-12 23:33:30 -0700

    created list_channels method

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,3 +1,6 @@
+require_relative 'channel'
+require_relative 'user'
+
 class Workspace
 
   attr_reader :users, :channels
@@ -5,6 +8,36 @@ class Workspace
   def intialize
     @users = []
     @channels = []
+
+  end
+
+  def list_channels
+    url = "https://slack.com/api/channels.list"
+
+    query_parameters = {
+      token: ENV['SLACK_API_TOKEN']
+    }
+
+    response = HTTParty.get(url, query: query_parameters)
+
+    # to do move this into recipient class
+    channels = response["channels"].map do |channel|
+      {:name => channel["name"],
+      :topic => channel["topic"]["value"],
+      :member_count => (channel["members"]).length,
+      :slack_id => channel["id"]}
+      # ["name"]
+    end
+    
+    tp channels
+  end
+
+  def list_users
+    url = "https://slack.com/api/users.list"
+
+    query_parameters = {
+      token: ENV['SLACK_API_TOKEN']
+    }
 
   end
 end
