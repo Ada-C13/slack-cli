fff6bf57a75ad5b8ae273039cb23fd38cf882687
Haben <haben7@gmail.com>
2020-03-12 18:20:07 -0700

    initialized channel, inherited from recipient , added a method

--- lib/channel.rb
+++ lib/channel.rb
@@ -7,32 +7,37 @@ require_relative 'recipient'
 Dotenv.load
 
 class Channel < Recipient
-  attr_reader :real_name, :status_text, :status_emoji
+  attr_reader :topic, :members
 
   BASE_URL = "https://slack.com/api/channels.list?"
   SLACK_TOKEN = ENV["SLACK_TOKEN"]
 
 
-  def initialize
-
-  
-
+  def initialize(id, name, topic, members)
+    super(id, name)
+    @topic = topic
+    @members = members
+    
   end
-
 
   def self.list_all
 
-  query = {
-    token: SLACK_TOKEN, 
-  }
+    query = {
+     token: SLACK_TOKEN, 
+   }
   
-  response = HTTParty.get(BASE_URL, query: query)
-
+    response = HTTParty.get(BASE_URL, query: query)
+    channel_list = []
+  
     response["channels"].each do |channel|
-    ap channel["name"]
+      name = channel["name"]
+      id = channel["id"]
+      topic = channel["topic"]["value"]
+      members = channel["members"].length
+      channel_list << Channel.new(id, name, topic, members)
     end
+    return channel_list
   end
  
 end
 
-puts Channel.list_all
