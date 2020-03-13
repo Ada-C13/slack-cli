7f9aab4c090cf694f7d729878f0698674fa96e7b
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 18:44:12 -0700

    added methods for selecting a user or channel

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -2,10 +2,43 @@ require_relative 'user'
 require_relative 'channel'
 
 class Workspace
-  attr_reader :users, :channels
+  attr_reader :users, :channels, :selected
   
   def initialize
     @users = User.list_all
     @channels = Channel.list_all
+    @selected = nil
+  end
+
+  def select_user(id)
+    # TODO: think about using reduce
+    found = users.find do |user|
+      user.name == id || user.slack_id == id
+    end
+
+    if found.nil?
+      puts "\nUser not found"
+      return nil
+    else
+      @selected = found
+      @selected.details
+      return @selected
+    end
+  end
+
+  def select_channel(id)
+    # TODO: think about using reduce
+    found = channels.find do |channel|
+      channel.name == id || channel.slack_id == id
+    end
+
+    if found.nil?
+      puts "\nChannel not found"
+      return nil
+    else
+      @selected = found
+      @selected.details
+      return @selected
+    end
   end
 end
