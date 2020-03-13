62aba6aea70006999b2b92bca84fe214b65cc7ba
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-12 20:28:43 -0700

    Added vcr to workspace

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -1,23 +1,60 @@
 require_relative "test_helper"
 
-describe "Initailize" do
-   before do
+describe "initialize" do
+  before do
+    VCR.use_cassette("workspace_creation") do
       @workspace = Workspace.new
     end
+  end
 
-  it "Creates a new instance of workspace" do
+  it "creates a new instance of Workspace" do
     expect(@workspace).must_be_kind_of Workspace
     expect(@workspace).must_respond_to :users
     expect(@workspace).must_respond_to :channels
   end
 
-  it "Returns empty array for users" do
+  it "initializes @users an array of users " do
     expect(@workspace.users).must_be_kind_of Array
-    expect(@workspace.users).must_be_empty 
+    expect(@workspace.users[0]).must_be_kind_of User
   end
 
-   it "Returns empty array for channels" do
+  it "initializes @channels an array of channels" do
     expect(@workspace.channels).must_be_kind_of Array
-    expect(@workspace.channels).must_be_empty 
+    expect(@workspace.channels[0]).must_be_kind_of Channel
   end
 end
+# it "initializes @selected an array of selected op" do
+#   @workspace = Workspace.new
+#   expect(@workspace.selected).must_be_nil
+# end
+
+# describe "list_channels" do
+#   before do
+#     @workspace = Workspace.new
+#   end
+#   it "returns an array of channel instances" do
+#   end
+#   it "can find the channels" do
+# VCR.use_cassette("list_channels") do
+#   channels = @workspace.list_channels
+
+#   expect(channels[0]).wont_be_nil
+#   expect(channels).must_be_kind_of Array
+#   expect(channels).must_equal ["general", "api-testing", "random"]
+# ["general", "api-testing", "random"]
+# expect(response["Seattle"][:lon]).must_equal "-122.3300624"
+# expect(response["Seattle"][:lat]).must_equal "47.6038321"
+
+# it "will raise an exception if the search fails" do
+#   VCR.use_cassette("location_find") do
+#     # location = ""
+#     # expect {
+#     #   response = get_location(location)
+#     # }.must_raise SearchError
+#   end
+
+#   it "lists the correct number of channel instances" do
+#   end
+#   it "lists the expected channels in the workspace" do
+#   end
+# end
