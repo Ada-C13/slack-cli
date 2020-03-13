e5feef49808adbbcd058d46c4840d4267a055267
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-12 19:32:20 -0700

    User- created initialize &list_user method

---
+++ lib/users.rb
@@ -0,0 +1,34 @@
+require "httparty"
+require "dotenv"
+require_relative "recipient"
+
+class User < Recipient
+  attr_accessor :real_name, :status_text, :status_emoji
+
+  def initialize(slack_id, name, real_name, status_text = nil, status_emoji = nul)
+    super(slack_id, name)
+    @real_name = real_name
+    @status_text = status_text
+    @status_emoji = status_emoji
+  end
+
+  def self.list_users
+    response = Recipient.get("users.list")
+    user_list = []
+    if response["ok"] != true
+      raise SlackAPI::SlackError, "There was an error. The error message is #{response["error"]}"
+    else
+      response["members"].each do |user|
+        @slack_id = user["id"]
+        @name = user["name"]
+        @real_name = user["real_name"]
+        @status_text = user["profile"]
+        @status_emoji = user["profile"]
+
+        user_list << User.new(@slack_id, @name, @real_name, @status_text,
+                              @status_emoji)
+      end
+    end
+    return user_list
+  end
+end #load_all method
