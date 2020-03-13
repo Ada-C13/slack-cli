206cbe0fba0a78917530e17b2736e3db948615ca
Nikki Vaughan <vnikki13@gmail.com>
2020-03-12 20:37:18 -0700

    tests for sending a message

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -21,5 +21,32 @@ describe 'Recipient class' do
         expect(response["members"][0]).must_include "id"
       end
     end
+
+    it 'raises an exception if there was any other response than a 200' do
+      expect{Recipient.get("htt://sack.com/pi/chanels.lis")}.must_raise Exception
+    end
+  end
+
+  describe 'send_message method sends a message to a user or channel' do
+    it 'sends a message to a user' do
+      VCR.use_cassette("sent_messages") do
+        slackbot = User.new(slack_id: "USLACKBOT", name: "slackbot", real_name:"Slackbot")
+        response = slackbot.send_message("Hello World")
+        expect(response.code).must_equal 200
+      end
+    end
+
+    it 'sends a message to a channel' do
+      VCR.use_cassette("sent_messages") do
+        random = Channel.new(slack_id: "CV86RT7AS", name: "random", member_count: "5", topic: "Non-work banter and water cooler conversation")
+        response = random.send_message("Hello World")
+        expect(random.send_message("Hello World").code).must_equal 200
+      end
+    end
+
+    it 'raises an exception if there was any other response than a 200' do
+      slackbot = User.new(slack_id: "USLABOT", name: "slaot", real_name:"Slakbot")
+      expect{slackbot.send_message("Hello World")}.must_raise Exception
+    end
   end
 end
