da6401e6149a6dd549254ab963b672692d7718be
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-13 16:02:00 -0700

    Recipent - SlackAPIError class added

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -7,10 +7,13 @@ class Recipient
   def self.get(sub_url)
     # self calls only know about its self to call in another method
     # must call the (Class name).something
-    # Recipent.get(querty) is called on the respective class that 
+    # Recipent.get(querty) is called on the respective class that
     url = "https://slack.com/api/#{sub_url}?token=#{SLACK_TOKEN}&pretty=1"
 
     response = HTTParty.get(url)
     return response
   end
+end
+
+class SlackAPIError < Exception
 end
