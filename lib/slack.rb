0816fca88a32324487ff49ca25da7c797dfc07d4
Katie <katie.vandervoort@gmail.com>
2020-03-12 21:06:04 -0700

    created methods for list options and get user input, and added logic for what to output depending on what user inputs

--- lib/slack.rb
+++ lib/slack.rb
@@ -14,10 +14,41 @@ def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
-  # TODO project
-  # tp Channel.list_all
+  list_options
+
+  choice = get_user_input
+
+  until choice == "quit"
+    if choice == "list channels"
+      tp workspace.channels
+    elsif choice == "list users"
+      tp workspace.users
+    end
+    puts "................."
+    list_options
+    choice = get_user_input
+  end
+
+
+
 
   puts "Thank you for using the Ada Slack CLI"
+end
+
+def list_options
+  puts "Please enter one of the following options: list users, list channels, or quit"
+end
+
+def get_user_input
+  user_input = gets.chomp
+  options = ["list users", "list channels", "quit"]
+
+  until options.include?(user_input)
+    puts "Sorry, I don't recognize that option. Please type list users, list channels, or quit"
+    user_input = gets.chomp
+  end
+
+  return user_input
 end
 
 main if __FILE__ == $PROGRAM_NAME
