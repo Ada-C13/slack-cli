62fbaa72e6a623a6132c0a5789509f4fcbee1382
Katie <katie.vandervoort@gmail.com>
2020-03-12 21:05:15 -0700

    created test for initializing recipient

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -1,1 +1,11 @@
 require_relative 'test_helper.rb'
+
+describe "Recipient" do
+  it "can initialize Recipient with slack id" do
+    slack_id = "UUT7ESHPU"
+    new_recipient = Recipient.new(slack_id)
+    expect(new_recipient).must_be_kind_of Recipient
+    expect(new_recipient.slack_id).must_equal slack_id
+  end
+
+end
