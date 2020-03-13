4275ba5ab94e534601444ad329083c32f70d8ff0
Nataliya Pogodina <n.pogodina@gmail.com>
2020-03-12 22:09:02 -0700

    Added CLI menu to slack.rb.

--- lib/slack.rb
+++ lib/slack.rb
@@ -4,19 +4,41 @@ require_relative "recipient"
 require_relative "user"
 require_relative "channel"
 
+def menu
+  puts "\nWhat would you like to do?"
+  puts "(please, type your selection)"
+  puts " >>> list users"
+  puts " >>> list channels"
+  puts " >>> quit"
+end
+
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
   puts "+++++++++++++++++++++++++++++"
   puts "Your workspace has #{workspace.users.count} users and #{workspace.channels.count} channels."
-  puts "+++++++++++++++++++++++++++++"
-  puts workspace.list_users
-  puts "+++++++++++++++++++++++++++++"
-  puts workspace.list_channels
+  
+  menu
+  input = gets.chomp
+
+  while input != "quit"
+    case input
+    when "list users"
+      puts workspace.list_users
+    when "list channels"
+      puts workspace.list_channels
+    when "quit"
+      puts "Thank you for using the Ada Slack CLI"
+      exit
+    else 
+      puts "Unknown command."
+    end
+
+    menu
+    input = gets.chomp
+  end
 
   # TODO project
-
-  puts "Thank you for using the Ada Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
