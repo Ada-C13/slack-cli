c92c8ff94139a4615e237baf08466a269dc766c7
Haben <haben7@gmail.com>
2020-03-12 18:21:16 -0700

    added print_channels method

--- lib/slack.rb
+++ lib/slack.rb
@@ -3,23 +3,33 @@ require 'dotenv'
 require 'httparty'
 require 'awesome_print'
 require_relative 'workspace'
+require_relative 'channel'
 
 # TODO project
 Dotenv.load
 
 
-
-
 def main
   puts "Welcome to the Ada Slack CLI!"
-  workspace = Workspace.new
-
-
-  
+  puts "Please choose from the following options:"
+  #workspace = Workspace.new
+  print_channels
 
 
 
   puts "Thank you for using the Ada Slack CLI"
 end
+  
+
+def print_channels
+  channels_list = Channel.list_all
+  channels_list.each do |channel|
+   puts channel.name + (', ') + channel.topic  + (', ') + channel.members.to_s  + (', ') + channel.id
+
+  end
+
+end
+
+ 
 
  main if __FILE__ == $PROGRAM_NAME
