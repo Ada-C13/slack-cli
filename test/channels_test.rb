6e27486c7a8128f6fd6554d4d2eb1c6a75862b77
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-12 21:44:09 -0700

    Channels - added 3 test

--- test/channels_test.rb
+++ test/channels_test.rb
@@ -1,14 +1,21 @@
 require_relative "test_helper"
 
-describe "Can instanitate a Channel" do
+#know need to to test intialize
+describe "Channel Class" do
   before do
     VCR.use_cassette("channel_list") do
-      @channel= Channel.list_channels
+      @channel = Channel.list_channels
     end
   end
 
-  it "Initialize " do
-  
+  it "Test List Channel method " do
+    #testing that we are return an Array
+    #testing that we are return a channel array
+    #test that the channel is count is an Integer
+    #there are currently 3 channels on 
+    expect(@channel).must_be_kind_of Array
     expect(@channel[0]).must_be_kind_of Channel
+    expect(@channel.length).must_be_kind_of Integer
+    expect(@channel.length).must_equal 3
   end
 end
