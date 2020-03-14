d689b8bc4da704eb085048ea14c469d2b5b26cd8
SuelyBarreto <suely@barreto.us>
2020-03-13 21:06:55 -0700

    DRY for check results

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -17,11 +17,8 @@ class Recipient
     @name = name
   end
 
-  # Method to send a message
-  def send_message(message)
-    query_parameters = { token: TOKEN, channel: @id, text: message }
-    result = HTTParty.post(SLACK_URL + "chat.postMessage", query: query_parameters)
-
+  # Check results
+  def self.check_results(result)
     unless result.code == 200
       raise RuntimeError, "Cannot talk to slack. HTTP code: #{result.code}"
     end
@@ -29,6 +26,13 @@ class Recipient
     unless result["ok"]
       raise RuntimeError, "Cannot talk to slack. Result is not ok."
     end
+  end
+  
+  # Method to send a message
+  def send_message(message)
+    query_parameters = { token: TOKEN, channel: @id, text: message }
+    result = HTTParty.post(SLACK_URL + "chat.postMessage", query: query_parameters)
+    self.check_results(result)
     return result
   end # def send_message
 
@@ -36,15 +40,7 @@ class Recipient
   def self.get(method)
     query_parameters = { token: TOKEN }
     result = HTTParty.get(SLACK_URL + method, query: query_parameters)
-
-    unless result.code == 200
-      raise RuntimeError, "Cannot talk to slack. HTTP code: #{result.code}"
-    end
-    
-    unless result["ok"]
-      raise RuntimeError, "Cannot talk to slack. Result is not ok."
-    end
-
+    self.check_results(result)
     return result
   end # def self.get
 
