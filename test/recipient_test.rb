17b642d9b6d9e1260dcd7ad33b6b1af45a1c87a9
Stephanie <stephaniejmars@gmail.com>
2020-03-13 19:48:39 -0700

    added recipient test shell

---
+++ test/recipient_test.rb
@@ -0,0 +1,17 @@
+require_relative 'test_helper'
+require_relative '../lib/user'
+
+describe "recipient" do 
+  describe "initialize" do 
+  
+  end
+
+  describe "self.get" do 
+
+  end
+
+  describe "send_message" do 
+  
+  end 
+
+end 
