05be3b54a6c74d8e823c1ec3a082aae5853eb73d
Jessica Stone <jstone4815@gmail.com>
2020-03-14 12:58:20 -0700

    add select_channel method to Workspace

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,20 +1,30 @@
+require_relative "user"
+require_relative "channel"
+
 class Workspace
   attr_reader :users, :channels
+  attr_writer :selected
   
   def initialize
-    @users = []
-    @channels = []
-    @selected
+    @users = User.list_all
+    @channels = Channel.list_all
+    @selected = ""
   end
 
-  def select_channel
+  def select_channel(input)
+    @channels.each do |channel|
+      if channel["id"].upcase == input || channel["name"].upcase == input
+        @selected = channel
+      end
+    end
+
   end
 
   def select_user
   end
 
   def show_details
-
+    return @selected.details
   end
 
   def send_message
