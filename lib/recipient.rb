3d076af4aea2dcd2cd7d7a04fbbf7a765e58b1cb
Stephanie <stephaniejmars@gmail.com>
2020-03-13 19:35:26 -0700

    set up recipient

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -5,7 +5,7 @@ require_relative 'channel'
 require_relative 'user'
 require_relative 'slack'
 
-Dotenv.load
+Dotenv.load # not needed bc its in slack and test helper 
 
 
 #list
@@ -15,8 +15,40 @@ Dotenv.load
 
 class Recipient
 
+  attr_reader :slack_id, :name 
 
+  def initialize
+    ## nn to initialize 
 
+  end 
+
+  def details 
+    raise NotImplimentedError, "Define this method in a child class"
+  end 
+
+  def self.get(url)
+    data = HTTParty.get(url, query: {token: ENV[SLACK_API_TOKEN]})
+    if data.code != 200 || data["ok"] == false 
+      raise SlackAPIError, "We encountered a problem: #{data["error"]}"
+    end 
+    return data 
+  end
+
+  def send_message(message)
+    ## nn to build 
+  end
+
+  def details 
+    raise NotImplimentedError, "Define this method in a child class"
+  end 
+
+  def self.list_all
+    raise NotImplimentedError, "Define this method in a child class"
+  end
+
+  class SlackAPIError < Exception
+    # i think this is it? lol 
+  end
 
 
 end
