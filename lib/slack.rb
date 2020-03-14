95f802cca868306c5334bdbfdd47cddfe4b0c3b6
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-13 19:13:21 -0700

    create command line interface with helper method for user input

--- lib/slack.rb
+++ lib/slack.rb
@@ -3,30 +3,42 @@ require_relative 'workspace'
 require_relative 'user'
 require_relative 'channel'
 
+# helper method
+def get_user_input
+  puts "Choose an action from the following list:
+  1. View Users => Enter: users
+  2. View Channels => Enter: channels 
+  3. Quit => Type: quit "
+  return gets.chomp.upcase
+end
+
 def main
-  puts "Welcome to the Ada Slack CLI!"
   workspace = SlackApi::Workspace.new
-
-   # TODO project
-
-  # interaction with command line
-  # list the users, the channels or quit
-
+  puts "\n"
+  puts "Welcome to the Ada Slack CLI!"
+  puts "\n"
   puts "What would you like to do?"
-  
+  puts "\n"
 
-  puts workspace.list_all_users
-  puts workspace.list_all_channels
+    user_input = get_user_input
+    puts "\n"
 
-  
-
- 
-
-
-
-
-  puts "Thank you for using the Ada Slack CLI"
+    while user_input != "QUIT"
+      if user_input == "USERS"
+        workspace.list_all_users
+        puts "\n"
+      elsif user_input == "CHANNELS"
+        workspace.list_all_channels
+        puts "\n"
+      else 
+        puts "Bad Entry: Try Again."
+        puts "\n"
+      end 
+      user_input = get_user_input
+      puts "\n"
+    end
+    puts "Thank you for using the Ada Slack CLI"
+    puts "\n"
 end
 
 main if __FILE__ == $PROGRAM_NAME
-
--- lib/workspace.rb
+++ lib/workspace.rb
@@ -29,7 +29,6 @@ module SlackApi
         token: ENV["SLACK_TOKEN"]
       }
        endpoint = BASE_URL + "users.list"
-
        response = HTTParty.get(endpoint, query: query_parameters)
        @users = response["members"].map do |member|
 
@@ -48,8 +47,7 @@ module SlackApi
         token: ENV["SLACK_TOKEN"]
       }
 
-    endpoint = BASE_URL + "channels.list"
-      # This list should include the channel's name, topic, member count, and Slack ID.
+      endpoint = BASE_URL + "channels.list"
       response = HTTParty.get(endpoint, query: query_parameters)
         @channels = response["channels"].map do |channel|
         Channel.new(slack_id: channel["id"], name: channel["name"],
@@ -60,7 +58,7 @@ module SlackApi
 # #############################################################################
 
     def list_all_users
-      tp @users
+      tp @users, "name", "real_name", "slack_id"
       return
      
     end
@@ -69,7 +67,7 @@ module SlackApi
     # #############################################################################
 
     def list_all_channels
-      tp @channels
+      tp @channels, "name", "topic", "member_count", "slack_id"
       return 
     end
       
@@ -84,10 +82,10 @@ module SlackApi
     # def select_channel
     # end
     
-    # def show_details
+    # def show_details (of either a channel or user)
     # end
     
-    # def send_msg
+    # def send_msg (to a user or a channel)
     # end
 
   end # class end
