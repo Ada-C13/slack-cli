3f719bad6abd595cbcfd719bf12cd8ac71ea86b4
dnguye2 <dnguyen1093@gmail.com>
2020-03-14 16:21:22 -0700

    tested and added list user method

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,36 @@
-class User
+require_relative 'recipient'
+
+class User < Recipient
   
+  attr_reader :real_name
+
+  def initialize(real_name:, name: , slack_id:)
+    super(name: name, slack_id: slack_id)
+
+    @real_name = real_name
+  end
+
+
+
+  def details
+  end
+
+  # =====Class methods=====
+  def self.list_all
+
+    response = User.get("https://slack.com/api/users.list")
+
+    # parsing the data
+
+    
+    users = response["members"].map do |item|
+      User.new(
+        real_name: item["real_name"],
+        name: item["name"],
+        slack_id: item["id"],
+      )
+    end
+
+    return users
+  end
 end
