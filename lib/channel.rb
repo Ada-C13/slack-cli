ea8f205d3d8b491f8af8d11fe7e5f7e2d9f12b3e
denisseai <msdenisseanaya@gmail.com>
2020-03-13 19:29:53 -0700

    Setting relationship between recipient- usr n chnl

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,6 +1,9 @@
-class Channel
+require_relative "recipient"
+
+class Channel < Recipient
   attr_reader :topic, :member_count
-  def initialize()
+  def initialize(topic:, member_count:, slack_id:, name:)
+    super(slack_id, name)
     @topic = topic
     @member_count = member_count
   end
--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,14 @@
-class User
+require_relative "recipient"
+
+class User < Recipient
   attr_reader :real_name, :status_text, :status_emoji
-  def initialize
+  def initialize(real_name:, status_text:, status_emoji:, slack_id:, name:)
+    super(slack_id, name)
     @real_name = real_name
     @status_text = status_text
     @status_emoji = status_emoji
   end
+  
   def details
     
   end
