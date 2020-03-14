839dc59fec05a3920634d9d6dd1eccebb0dcd000
cojenco <icathyouyang@gmail.com>
2020-03-13 17:08:21 -0700

    Removed require_relative lib files to the individual tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,17 +6,11 @@ end
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# require 'minitest/skip_dsl'
 require 'vcr'
 require 'webmock/minitest'
 require 'dotenv'
 Dotenv.load
 
-require_relative '../lib/recipient'
-require_relative '../lib/user'
-require_relative '../lib/channel'
-require_relative '../lib/slack_api_error'
-require_relative '../lib/workspace'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
