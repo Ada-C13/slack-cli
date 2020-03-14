23ce4515875d52f28cd199a13a4c867e433c80ab
Jessica Stone <jstone4815@gmail.com>
2020-03-14 12:57:30 -0700

    add user options for CLI

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,21 +1,57 @@
 #!/usr/bin/env ruby
-require 'dotenv'
-require 'httparty'
 require_relative 'workspace'
+require 'dotenv'
 
 Dotenv.load
-BASE_URL = "https://slack.com/api/conversations.list"
-SLACK_TOKEN = ENV["SLACK_TOKEN"]
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
-  response = HTTParty.get(BASE_URL, query: {token: SLACK_TOKEN})
+  puts "This workspace has #{workspace.users.length} users and #{workspace.channels.length} channels"
 
-  # response["channels"].each do |channel|
-  #   puts channel["name"]
-  # end
+  # given three options for how to interact with the program:
+  input = ""
+  until input == "quit"
+    puts "\nWhat would you like to do?"
+    options = ["list users", "list channels", "select user", "select channel", "details", "quit"]
+
+    options.each_with_index do |option, i|
+      puts "#{i + 1}. #{option}"
+    end
+
+    input = gets.chomp.downcase
+
+    case input
+      when "1", "list users"
+        puts workspace.users
+      when "2", "list channels"
+        puts workspace.channels
+      when "3", "select user"
+        print "\nProvide the username or Slack ID of the user you want to select: "
+        user = gets.chomp.upcase
+
+      when "4", "select channel"
+        print "\nprovide the channel name or Slack ID of the channel you want to select: "
+        channel = gets.chomp.upcase
+
+      when "5", "details"
+
+      when "6", "quit"
+        input = "quit"
+      else
+        puts "That's not a valid option, please try again."
+    end
+  end
+
+  # list users
+  # list channels
+  # quit
+  # As a user who is at the program's input prompt...
+  
+  # When I type quit, the program should exit.
+  # After completing any command other than quit, the program should reprint the list of commands and ask for another input.
+
 
   puts "Thank you for using the Ada Slack CLI"
 end
