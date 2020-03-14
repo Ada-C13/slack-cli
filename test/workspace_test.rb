58968f42c910a3e9e9c56f7888ba57669f3f69ad
Jessica Stone <jstone4815@gmail.com>
2020-03-14 14:19:13 -0700

    add tests for Workspace - initialize, select_channel, select_user

---
+++ test/workspace_test.rb
@@ -0,0 +1,57 @@
+require_relative 'test_helper'
+require "./lib/workspace"
+
+describe "Workspace" do
+  before do
+    workspace = ''
+    VCR.use_cassette("workspace-data") do
+      @workspace = Workspace.new
+    end
+  end
+
+  describe "initialize" do
+    it "initializes Workspace object with an array of users and channels" do
+
+      expect(@workspace).must_be_kind_of Workspace
+      expect(@workspace.users).must_be_kind_of Array
+      expect(@workspace.channels).must_be_kind_of Array
+    end
+  end
+
+  describe "select_channel" do
+    it "sets @selected to correct Channel instance when given an ID" do
+      @workspace.select_channel("slack-cli")
+      
+      expect(@workspace.selected).must_be_kind_of Channel
+      expect(@workspace.selected.slack_id).must_equal "slack-cli"
+    end
+
+    it "sets @selected to correct Channel instance when given a username" do
+      @workspace.select_channel("CV5KNMDKN")
+      
+      expect(@workspace.selected).must_be_kind_of Channel
+      expect(@workspace.selected.name).must_equal "CV5KNMDKN"
+    end
+  end
+  
+  describe "select_user" do
+    it "sets @selected to correct User instance when given an ID" do
+      @workspace.select_user("USLACKBOT")
+      
+      expect(@workspace.selected).must_be_kind_of User
+      expect(@workspace.selected.slack_id).must_equal "USLACKBOT"
+    end
+
+    it "sets @selected to correct User instance when given a username" do
+      @workspace.select_user("slackbot")
+      
+      expect(@workspace.selected).must_be_kind_of User
+      expect(@workspace.selected.name).must_equal "slackbot"
+    end
+
+  end
+
+  describe "show_details" do
+    
+  end
+end
