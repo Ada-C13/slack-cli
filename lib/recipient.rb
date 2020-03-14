a491fecec693d01c7cce61d0931d4b050585619e
dnguye2 <dnguyen1093@gmail.com>
2020-03-14 16:20:36 -0700

    tested and added list channels method

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,3 +1,8 @@
+require "httparty"
+require 'dotenv'
+
+Dotenv.load
+
 class Recipient
   
   attr_reader :slack_id, :name
@@ -7,16 +12,27 @@ class Recipient
     @name = name
   end
 
- def send_message(text)
- end
+  def send_message(text)
+  end
 
- def details
- end
+  def details
+  end
 
 
- # Class methods
+# =====Class methods=====
 
+# gets url, based off of API documentation
  def self.get(url)
+  # send message using HTTParty
+  response = HTTParty.get(url, query: {token: ENV['SLACK_TOKEN']})
+
+  # accounting for errors
+  # slack still gives 200 code
+  if response.code != 200 || response["ok"] == false
+    raise SlackAPIError, "We encountered a problem: #{response["error"]}"
+  end
+
+  return response
  end
 
  def self.list_all
