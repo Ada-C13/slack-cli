ab44620523f2672fed60fa4d6309e1aef60f3b17
Stephanie <stephaniejmars@gmail.com>
2020-03-13 20:21:14 -0700

    updated channel to have self.get and initialize.

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,5 +1,6 @@
 require 'HTTParty'
 require 'dotenv'
+require 'table_print'
 require_relative 'workspace'
 require_relative 'slack'
 require_relative 'user'
@@ -15,8 +16,46 @@ Dotenv.load
 
 class Channel < Recipient
 
+  attr_reader :topic, :member_count
+  def initialize(name:, slack_id:, topic:, member_count:)
+    super(slack_id, name)
+    @topic = topic
+    @member_count = member_count 
+  end 
 
+  # get the data
+  data = self.get("https://slack.com/api/channels.list")
 
-
+  #parse the data, instantiate an object 
+  channels = []
+  data["channels"].each do |channel|
+    channels << self.new(
+              name: channel["name"],
+              slack_id: channel["id"],
+              topic: channel["topic"]["value"], 
+              member_count: channel["num_members"]       
+    )
+  end
+  return users
 
 end
+
+# def list_channels
+#   query = {
+#     token: ENV["SLACK_API_TOKEN"]
+#   }
+  
+#   url = "https://slack.com/api/channels.list"
+#   response = HTTParty.get(url, query: query)
+  
+#   response["channels"].each do |channel|
+#     puts "Channel info:
+#       name: #{channel["name"]},
+#       topic: #{channel["topic"]["value"]},
+#       member count: #{channel["num_members"]},
+#       id: #{channel["id"]},
+#       "
+#   end 
+# end
+
+
