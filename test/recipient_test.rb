8e6d195da18c01086be7f703c40c7990920c92c8
Jessica Stone <jstone4815@gmail.com>
2020-03-14 13:14:51 -0700

    add tests for Recipient class

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -8,7 +8,20 @@ describe "Recipient" do
     @recipient = Recipient.new(slack_id, name)
   end
 
-  it "can create a Recipient class" do
-    expect(@recipient).must_be_kind_of Recipient
+  describe "initialize" do
+    it "can create a Recipient class" do
+      expect(@recipient).must_be_kind_of Recipient
+    end
   end
+
+  describe "self.get" do
+    it "should get correct details" do
+      VCR.use_cassette("user-details") do
+        response = @user.details
+        expect(@user.real_name).must_equal "Slackbot"
+        expect(@user.name).must_equal "slackbot"
+      end
+    end
+  end
+
 end
