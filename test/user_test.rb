1d3a5fb3d691839fb669598f2d752434250b614d
Jessica Stone <jstone4815@gmail.com>
2020-03-14 13:15:52 -0700

    add tests for User class

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,32 +1,37 @@
 require_relative 'test_helper'
 require "./lib/user"
 
-describe "Recipient" do
-  before do 
-    slack_id = "USLACKBOT"
-    @user = User.new(slack_id)
-  end
+describe "User" do
+  describe "details" do
+  end  
 
-  describe "initialize" do
-    it "can create a User class" do
-      expect(@user).must_be_kind_of User
+  describe "self.get" do
+    it "gets a list of users" do
+      result = {}
+      VCR.use_cassette("users-list") do
+        result = User.get("users.list")
+      end
+
+      expect(result).must_be_kind_of HTTParty::Response
+      expect(result["ok"]).must_equal true
+    end
+
+    it "raises an error when the call fails" do
+      VCR.use_cassette("users-list-fail") do
+        expect{User.get("usersfail.list")}.must_raise SlackApiError
+      end
     end
   end
 
-  describe "details" do
-    it "should update to correct details" do
+  describe "self.list_all" do
+    it "returns a list all users" do
       VCR.use_cassette("user-details") do
-        response = @user.details
-        expect(@user.real_name).must_equal "Slackbot"
-        expect(@user.name).must_equal "slackbot"
+        response = User.list_all
+        response.each do |user|
+          expect(user).must_be_kind_of User
+          expect(user).must_respond_to :slack_id
+        end
       end
     end  
   end
-
-
-  describe "self.list_all" do
-    # loop through each user
-
-  end
-
 end
