1e0c1f77645219297528a5c8b04e1e0a0062f9f4
Rosalyn <syrosalynyu@gmail.com>
2020-03-14 20:51:28 -0700

    1.) move #prompt_options method from Workspace to slack.rb 2.) construct the while loop in #main in slack.rb. Return value will base on user input by calling the related methods from different classes.

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,34 +1,48 @@
 #!/usr/bin/env rubycc
 require "httparty"
 require "dotenv"
+require "table_print"
 require_relative "workspace"
 
+
 Dotenv.load
+
+def prompt_options
+  puts "6 options: list users, list channels, select user, select channel, details, or quit:"
+  input = gets.chomp.downcase
+  return input
+end
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
-  # Print users/channels
-  puts "3 options: list users, list channels, or quit:"
-  input = gets.chomp
-  
-  while input != "quit"
-    # puts "you didn't quit"
-    if input == "list users"
-      puts workspace.users
-    elsif input == "list channels"
-      puts workspace.channels
+
+  input = prompt_options
+  while input != "quit" && input != "exit"
+    case input
+    when "list users"
+      tp workspace.users, "slack_id", "name", "real_name"
+    when "list channels"
+      tp workspace.channels, "slack_id", "name", "topic", "member_count"
+    when "select user"
+      puts "Please provide user ID or username:"
+      user_id_or_username = gets.chomp
+      validate_user = workspace.select_user(user_id_or_username)
+      puts "User doesn't exist!" if validate_user == nil
+    when "select channel"
+      puts "Please provide channel ID or channel name:"
+      channel_id_or_channelname = gets.chomp
+      validate_channel = workspace.select_channel(channel_id_or_channelname)
+      puts "Channel doesn't exist!" if validate_channel == nil
+    when "details"
+      puts workspace.show_details
+      puts "You haven't selected a user or channel." if workspace.show_details == ""
     else 
       puts "try again"
     end
-    puts "3 options: list users, list channels, or quit:"
-    input = gets.chomp
+    input = prompt_options
   end
-
-
-
   puts "Thank you for using the Ada Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
-
