dfdb79536c18f78cf50195f255fb4796057f9297
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-03-15 15:56:32 -0700

    Added newlines to CLI prompts for legibility

--- lib/slack.rb
+++ lib/slack.rb
@@ -11,35 +11,35 @@ require_relative "channel"
 Dotenv.load # must run from root folder: ruby lib/slack.rb
 
 def main
-  puts "Welcome to the Ada Slack CLI!".blue
+  puts "Welcome to the Ada Slack CLI!\n".blue
   workspace = Slack::Workspace.new
 
-  options_message = "Please choose from the following options:\nlist users, list channels, select user, select channel, details, send message, or quit".blue
+  options_message = "Please choose from the following options:\n1. list users \n2. list channels \n3. select user \n4. select channel \n5. details \n6. send message \n7. quit\n".blue
 
   puts options_message
   input = gets.chomp.downcase
 
-  until input == "quit"
+  until input == "quit" || input == "7"
     case input
-    when "list users"
+    when "list users", "1"
       tp workspace.users, "slack_id", "name", "real_name"
-    when "list channels"
+    when "list channels", "2"
       tp workspace.channels, "slack_id", "name", "topic", "member_count"
-    when "select user"
+    when "select user", "3"
       puts "Please enter user name or ID:".blue
       search_term = gets.chomp
       puts workspace.select_user(search_term)
-    when "select channel"
+    when "select channel", "4"
       puts "Please enter channel name or ID:".blue
       search_term = gets.chomp
       puts workspace.select_channel(search_term)
-    when "details"
+    when "details", "5"
       if workspace.selected == nil
         puts "None selected. Please select user or channel".red
       else
         workspace.show_details
       end
-    when "send message"
+    when "send message", "6"
       if workspace.selected == nil
         puts "None selected. Please select user or channel".red
       else
@@ -49,6 +49,7 @@ def main
         puts "Message sent".green
       end
     end
+    puts "\n"
     puts options_message
     input = gets.chomp.downcase
   end
