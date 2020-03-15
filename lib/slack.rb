63c4d40570e368f85f8b132f2876a8d0b3d7657f
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-14 17:51:38 -0700

    cleaned notes on slack rb.

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,16 +1,13 @@
 #!/usr/bin/env ruby
 require "dotenv"
-require "pry"
-require "httparty"
+require "table_print"
 
 Dotenv.load
 
 require_relative "workspace"
-require_relative "user"
-require_relative "channel"
 
 def menu
-  # should be able to 3 things Wave 1
+  # should be able to do 3 things Wave 1
   puts "Menu"
   puts "1. List Channels"
   puts "2. List Users"
@@ -21,32 +18,39 @@ def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
   # i is set to negative -1
-  # as long a user of the program doesn't chose to "quit" the program
-  # it will countine to loop thru everything until a user quits
+  # as long a user of the program doesn't chose to "quit" the program will countine to run thru the while loop
+  # i will then become 1 which is more than 1 and will end the program
+
+  ### need to rescue the Invalid Response error
   i = -1
   while i < 0
     puts "Your workspace has #{workspace.channels.length} channels and #{workspace.users.length} users."
     puts "--------------------"
     puts "What would you like to do? 'list channels', 'list users', or 'quit'"
-    answer = gets.chomp.downcase
-     unless answer == "list channels" || answer == "user channels" || answer == "quit"
-      raise Exception.new "Please enter a valid response"
-    if answer == "list channels"
-      # puts workspace.list_channels (method that shows channels)
-      puts workspace.list_channels
-    elsif answer == "list users"
-      # puts workspace.list_users
-      puts workspace.list_users
-    elsif answer == "quit"
-      # need to quit
-      i = 1
-    end #if loop
-    puts "Thank you for using the Ada Slack CLI"
+      answer = gets.chomp.downcase
+      begin
+       if answer != "list channels" || answer != "user channels" || answer != "quit"
+        raise StandardError.new "This exception will be rescued!"
+      end 
+      rescue StandardError => exception
+        p exception.message
+      end
+
+        if answer == "list channels"
+          # puts workspace.list_channels (method that shows channels)
+          tp workspace.channels, :topic, :member_count
+        elsif answer == "list users"
+          # puts workspace.list_users
+          tp workspace.users, :slack_id, :real_name, :name
+        elsif answer == "quit"
+          # need to quit
+          i = 1
+        end #if loop
+     puts "Thank you for using the Ada Slack CLI"
   end #while loop
 end # main
-end
-main if __FILE__ == $PROGRAM_NAME
 
+main if __FILE__ == $PROGRAM_NAME
 #  unless answer == "list channels" || answer == "user channels" || answer == "quit"
 #       raise Exception.new "Please enter a valid response"
 # end
