f52fa2753d985497b0268050a64aac7a9dea724d
denisseai <msdenisseanaya@gmail.com>
2020-03-14 19:47:21 -0700

    Deleted completed todo comments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,9 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
+require 'dotenv'
+
+Dotenv.load
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -17,10 +20,10 @@ VCR.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "test/cassettes" # folder where casettes will be located
-  config.hook_into :webmock # tie into this other tool called webmock
+  config.cassette_library_dir = "test/cassettes"
+  config.hook_into :webmock 
   config.default_cassette_options = {
-    :record => :new_episodes,    # record new data when we don't have it yet
+    :record => :new_episodes,
     :match_requests_on => [:method, :uri, :body], # The http method, URI and body of a request all need to match
   }
   config.filter_sensitive_data("<SLACK_TOKEN>") do
