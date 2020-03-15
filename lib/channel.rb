bb0fb93b57c0e59d87dc2555d2ef75c0124ebeff
denisseai <msdenisseanaya@gmail.com>
2020-03-14 19:11:17 -0700

    Added channel list_all method

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,18 +1,34 @@
 require_relative "recipient"
 
 class Channel < Recipient
-  attr_reader :topic, :member_count
-  def initialize(topic:, member_count:, slack_id:, name:)
-    super(slack_id, name)
+  attr_reader :topic, :num_members
+
+  CHANNEL_URL = "https://slack.com/api/channels.list"
+
+  def initialize(topic:, num_members:, slack_id:, name:)
+    super(slack_id: slack_id, name: name)
     @topic = topic
-    @member_count = member_count
+    @num_members = num_members
   end
+
   def details
-    
+    response = self.get(CHANNEL_URL, {token: SLACK_TOKEN})
   end
+
   def self.list_all
     # Use specific api endpoint
     # error handling? 
-    
+    #raise Exception.new(response['error']) unless response["ok"]
+    response = self.get(CHANNEL_URL)
+    channels = []
+    response["channels"].each do |channel| # maybe do map here
+      channels << Channel.new(
+        topic: channel['topic']['value'],
+        num_members: channel['num_members'],
+        slack_id: channel['id'],
+        name: channel['name']
+      )
+    end
+    return channels
   end
 end
