21e3c2163d7def7bed60b1383f976b05fa25a71e
Haben <haben7@gmail.com>
2020-03-14 23:09:43 -0700

    added Dotenv.load

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,9 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
-require_relative '..lib/workspace'
+require 'dotenv'
+
+Dotenv.load
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -25,8 +27,13 @@ VCR.configure do |config|
     :match_requests_on => [:method, :uri, :body], # The http method, URI and body of a request all need to match
   }
 
+
   # Don't leave our token lying around in a cassette file.
   config.filter_sensitive_data("<HABEN_TOKEN>") do
     ENV["SLACK_TOKEN"]
+  
+  # config.filter_sensitive_data("<Bot_TOKEN>") do
+  #   ENV["Bot_TOKEN"] 
   end
+
 end
