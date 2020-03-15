a0597c285b078f749930e61156e7461cb10f1ba3
Nikki Vaughan <vnikki13@gmail.com>
2020-03-15 16:18:50 -0700

    Refactored and added an inherited SlackAPIError from Exception

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,6 +1,4 @@
 require 'httparty'
-require 'dotenv'
-Dotenv.load
 
 class Recipient
   attr_reader :slack_id, :name
@@ -10,26 +8,36 @@ class Recipient
     @name = name
   end
 
-  def self.get(url)
-    params = {
-      token: ENV["SLACK_TOKEN"]
-    }
-    response = HTTParty.get(url, query: params)
-    raise Exception if response.code != 200
-    return response
-  end
-
   def send_message(message)
-    puts "#{message} #{slack_id}"
-    url = "https://slack.com/api/chat.postMessage"
     params = {
       token: ENV["SLACK_TOKEN"],
       channel: slack_id,
       text: message
     }
 
-    response = HTTParty.post(url, query: params)
-    raise Exception if response.code != 200
+    response = HTTParty.post("https://slack.com/api/chat.postMessage", query: params)
+    if response.code != 200 || response["ok"] == false
+      raise SlackAPIError, "We encountered a problem: #{response["error"]}"
+    end
     return response
   end
+
+  def self.get(url)
+    params = {
+      token: ENV["SLACK_TOKEN"]
+    }
+    data = HTTParty.get(url, query: params)
+    if data.code != 200 || data["ok"] == false
+      raise SlackAPIError, "We encountered a problem: #{data["error"]}"
+    end
+
+    return data
+  end
+
+  def self.list_all
+    raise NotImplementedError, "Define this method in a child class"
+  end
+end
+
+class SlackAPIError < Exception
 end
