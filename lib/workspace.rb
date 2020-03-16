7ba766ed60f4cb06407c7f33b7b7427d854590b4
thenora <thenora@gmail.com>
2020-03-16 01:26:31 -0700

    workspace select channel and tests

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -7,14 +7,17 @@ class Workspace
     @selected = nil
   end
 
-  def select_channel
-    user_input = gets.chomp.upcase
+  def select_channel(search_word)
+    user_input = search_word.upcase
     @channels.each do |channel|
       if channel.name.upcase == user_input || channel.slack_id.upcase == user_input
         @selected = channel
         return "#{@selected.name} is now selected."
       end
     end
+
+    @selected = nil
+    return "Sorry, I couldn't find a channel matching \"#{search_word}.\""
   end
 
   def select_user
--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -1,1 +1,39 @@
-require_relative 'test_helper'
+require_relative "test_helper"
+require_relative "../lib/workspace"
+require_relative "../lib/user"
+require_relative "../lib/channel"
+
+describe "Workspace class" do
+  before do
+    VCR.use_cassette("workspace") do
+      @workspace_inst = Workspace.new
+    end
+  end
+
+  describe "initialize" do
+    it "is an instance of Workspace" do
+      expect(@workspace_inst).must_be_kind_of Workspace
+    end
+
+    it "has a list of users" do
+      expect(@workspace_inst.users).must_be_kind_of Array
+      expect(@workspace_inst.users[0]).must_be_kind_of User
+    end
+
+    it "has a list of channels" do
+      expect(@workspace_inst.channels).must_be_kind_of Array
+      expect(@workspace_inst.channels[0]).must_be_kind_of Channel
+    end
+  end
+
+  describe "select_channel" do
+    it "selects a channel from a slack_id" do
+      user_input = "CV7V4KYLF"
+      @workspace_inst.select_channel(user_input)
+
+      expect(@workspace_inst.selected.name).must_equal "random"
+      expect(@workspace_inst.selected).must_be_kind_of Channel
+    end
+  end
+
+end
