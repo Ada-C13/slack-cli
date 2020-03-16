c0af31b500bb0c4f5ba3fced6566065262d806f6
Vera <lizeth.veraro@outlook.com>
2020-03-15 22:44:22 -0700

    Raises and SlackApi class error when API call fails

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,5 +1,6 @@
 require "httparty"
 require "awesome_print"
+
 require "dotenv"
 Dotenv.load
 
@@ -11,14 +12,14 @@ module Slack_cli
         @id = id
         @name = name
       end
-      # Given the URL and params(KEY) get the response from the API. 
 
+      # Given the URL and params(KEY) get the response from the API. 
       def self.get(url,params)
         resp = HTTParty.get(url, query: params)
         if resp.code == 200 && resp.parsed_response["ok"]
           return resp
         end
-        return false
+        raise SlackAPIError, "We encountered a problem: #{resp["error"]}" 
       end
 
       def self.list_all
@@ -30,8 +31,6 @@ module Slack_cli
       end
   end
 end
-# base_url = "https://slack.com/api/"
-#     post_url = "#{base_url}channels.list"
-#     params = { token: ENV["SLACK_API_TOKEN"] }
 
-# Recipient.get(post_url, params)
+class SlackAPIError < Exception
+end
