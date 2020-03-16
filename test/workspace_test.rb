93b4ce4830def42e710b47e9d8c56d370d8f1379
Haben <haben7@gmail.com>
2020-03-16 15:30:54 -0700

    added methods and edited some

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -11,36 +11,110 @@ describe "Workspace instantiation" do
   end
 end
 
-describe "show_details" do
-  VCR.use_cassette("workspace-show-details") do
-    @workspace = Workspace.new
+
+describe "Show Details" do
+  it "Will return a table print of details" do
+    VCR.use_cassette("workspace-show-details") do
+      workspace = Workspace.new
+ 
+      new_user = workspace.select_user("USLACKBOT")
+
+      expect(workspace.show_details).must_be_kind_of TablePrint::Returnable
+    end
   end
 
-  new_user = @workspace.select_user("habenup")
-
+  
   it "Will return a table print of details" do
-    expect(new_user.details).must_be_kind_of TablePrint::Returnable
-  end
+    VCR.use_cassette("workspace-show-details") do
+      workspace = Workspace.new
+      new_channel = workspace.select_channel("random")
 
-  new_channel = @workspace.select_channel("random")
-
-  it "Will return a table print of details" do
-    expect(new_channel.details).must_be_kind_of TablePrint::Returnable
+      expect(workspace.show_details).must_be_kind_of TablePrint::Returnable
+    end 
   end
 end
 
-describe "Select User" do
 
-  VCR.use_cassette("workspace-select-user") do
-    @workspace = Workspace.new
+describe "Select a specific user" do
+  
+  it "Select a user" do
+    VCR.use_cassette("workspace-select-User") do
+      @workspace = Workspace.new
+    end
+    @selected = @workspace.select_user("habenup")
+    expect(@workspace.selected).must_be_instance_of User
+
+    @selected = @workspace.select_user("UUW7LV8TB")
+    expect(@workspace.selected).must_be_instance_of User
+   end
+
+   it "If no user is found returns nil" do
+    VCR.use_cassette("workspace-select-User") do
+      @workspace = Workspace.new
+    end
+
+    @selected = @workspace.select_user("test")
+    expect(@workspace.selected).must_be_nil
+  end
+end
+
+
+describe "Select a specific channel" do
+  
+  it "Select a channel" do
+    VCR.use_cassette("workspace-select-Channel") do
+      @workspace = Workspace.new
+    end
+    @selected = @workspace.select_channel("random")
+    expect(@workspace.selected).must_be_instance_of Channel
+
+    @selected = @workspace.select_channel("CV6AN643F")
+    expect(@workspace.selected).must_be_instance_of Channel
   end
 
-  new_user = @workspace.select_user("habenup")
-  p new_user
+  it "if no channel is found returns nil" do
+    VCR.use_cassette("workspace-select-Channel") do
+      @workspace = Workspace.new
+    end
 
-  it "Will return select user" do
-    expect(User.name).must_be_kind_of String
+    @selected = @workspace.select_channel("test")
+    expect(@workspace.selected).must_be_nil
+  end
+end
+
+
+describe "Send Message" do
+
+  it "returns true if the message is sent to user" do
+    VCR.use_cassette("workspace-select-User") do
+      workspace = Workspace.new
+      workspace.select_user("habenup")
+      expect(workspace.selected).must_be_instance_of User
+
+      response = workspace.send_message("Test message")
+      expect(response).must_equal true
+    end
   end
 
+  it "returs true if the message is sent to channel" do
+    VCR.use_cassette("workspace-select-Channel") do
+      workspace = Workspace.new
+
+      workspace.select_channel("random")
+      expect(workspace.selected).must_be_instance_of Channel
+
+      response = workspace.send_message("Test message")
+      expect(response).must_equal true
+    end
+  end
+
+  it "returns nil if there is no user or channel selected" do 
+    VCR.use_cassette("workspace-select-Channel") do
+      workspace = Workspace.new
+
+      response = workspace.send_message("Test message")
+      expect(response).must_equal nil
+    end 
+  end
 end
 
