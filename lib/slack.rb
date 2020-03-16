739ee76da2f1511d45c4217fbdaa32d95ac6676e
Yaz <yasmineoshaugh@gmail.com>
2020-03-15 23:24:14 -0700

    Wave 1 complete

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,24 +1,26 @@
 require_relative 'workspace'
-require 'dotenv'
-Dotenv.load
-API_KEY = ENV['SLACK_TOKEN']
-require 'HTTParty'
-base_url = 	'https://slack.com/api/channels.list'
-query_param = {
-  token: API_KEY
-}
-channel_list = HTTParty.get(base_url, query: query_param)
-require 'ap'
-channel_list.parsed_response["channels"].each do |channel|
-  ap bananna["name"]
-end
-
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
-
-  # TODO project
+  puts "This workspace has #{workspace.users.length} users and #{workspace.channels.length} channels."
+  input = ""
+  # TO DO: refactor to meet requirements of typing out options (wave 1)
+  while input != "quit"
+    puts "\nWhat would you like to do?"
+    puts "list users \nlist channels \nquit"
+    input = gets.chomp.downcase
+    case input 
+    when "list users"
+      workspace.print_list("users")
+    when "list channels"
+      workspace.print_list("channels")
+    when "quit"
+      puts "Goodbye!"
+    else
+      puts "Oops, that's not valid input."
+    end
+  end
 
   puts "Thank you for using the Ada Slack CLI"
 end
