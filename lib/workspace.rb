977769ef064a740e12a067271ef78f05bd143f14
thenora <thenora@gmail.com>
2020-03-16 00:48:18 -0700

    working on select channel

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -2,12 +2,16 @@ class Workspace
   attr_reader :users, :channels, :selected
 
   def initialize
-    @users = []
-    @channels = []
+    @users = User.list_all
+    @channels = Channel.list_all
+    @selected = ""
   end
 
   def select_channel
-
+    user_input = gets.chomp
+    @channels.each do |channel|
+      @selected = channel if channel.name == user_input || channel.slack_id == user_input
+    end
   end
 
   def select_user
