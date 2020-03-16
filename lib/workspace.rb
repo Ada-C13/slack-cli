06ff7f3d3aeb0570a753cc992c516291b9f3b9f4
Vera <lizeth.veraro@outlook.com>
2020-03-15 22:47:01 -0700

    Select_user and channel methods, show details added

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,11 +1,48 @@
+# worksapce.rb
+require_relative "user"
+require_relative "channel"
 
 module Slack_cli
   class Workspace
-    attr_reader :users, :channels
+    attr_reader :users, :channels, :selected
   
     def initialize
-      @users = []
-      @channels = []
+      @users = Slack_cli::User.list_all
+      @channels =  Slack_cli::Channel.list_all
+      @selected = nil
     end
+
+    def select_channel
+      user_input = gets.chomp
+      channels.each do |channel|
+        if user_input == channel.name || user_input == channel.id
+          @selected = channel
+          return @selected.name
+        end
+      end
+      return @selected = nil
+    end
+
+    def select_user
+
+      user_input = gets.chomp
+
+      users.each do |user|
+        if user_input == user.real_name || user_input == user.id
+          @selected = user
+          return @selected.name
+        end
+      end
+      return @selected = nil
+    end
+
+    def show_details
+      @selected.details      
+    end
+
+    def send_message
+    end
+
   end
 end
+
