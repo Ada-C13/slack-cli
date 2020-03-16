5581159a6a6483942cf6bcfb52ecc68192811f6e
thenora <thenora@gmail.com>
2020-03-16 03:31:00 -0700

    wave 2 cleanup

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,7 +1,4 @@
-require 'httparty'
-require "dotenv" # ? do I need this?
-Dotenv.load # ? do I need this?
-
+require "httparty"
 
 class Recipient
   attr_reader :name, :slack_id
@@ -11,10 +8,8 @@ class Recipient
     @slack_id = slack_id
   end
 
-  
   def send_message(message)
-    
-    response = HTTParty.post("https://slack.com/api/chat.postMessage", query: {token: ENV['BOT_TOKEN'], channel: self.slack_id, text: message })
+    response = HTTParty.post("https://slack.com/api/chat.postMessage", query: { token: ENV["BOT_TOKEN"], channel: self.slack_id, text: message })
 
     if response.code != 200 || response["ok"] == false
       raise SlackAPIError, "We encountered a problem: #{response["error"]}"
@@ -22,24 +17,22 @@ class Recipient
   end
 
   def self.get(url)
-    response = HTTParty.get(url, query: {token: ENV['SLACK_TOKEN']})
+    response = HTTParty.get(url, query: { token: ENV["SLACK_TOKEN"] })
 
     if response.code != 200 || response["ok"] == false
       raise SlackAPIError, "We encountered a problem: #{response["error"]}"
     end
 
     return response
-
   end
-  
+
   def details
-    raise NotImplementedError, 'Implement me in a child class!'
+    raise NotImplementedError, "Implement me in a child class!"
   end
 
   def self.list_all
-    raise NotImplementedError, 'Implement me in a child class!'
+    raise NotImplementedError, "Implement me in a child class!"
   end
-
 end
 
 class SlackAPIError < Exception
--- lib/user.rb
+++ lib/user.rb
@@ -25,68 +25,6 @@ class User < Recipient
   end
 
   def details
-    tp self, "slack_id", "name", "real_name"
+    tp self, "name", "real_name", "status_text", "status_emoji", "slack_id"
   end
 end
-
-# Sample output
-
-# "members": [
-#   {
-#       "id": "USLACKBOT",
-#       "team_id": "TV63QKAAU",
-#       "name": "slackbot",
-#       "deleted": false,
-#       "real_name": "Slackbot",
-#       "profile": {
-#           "real_name": "Slackbot",
-#           "real_name_normalized": "Slackbot",
-#           "display_name": "Slackbot",
-#           "display_name_normalized": "Slackbot",
-#           "fields": null,
-#           "status_text": "",
-#           "status_emoji": "",
-#           "first_name": "slackbot",
-#           "last_name": "",
-#           "status_text_canonical": "",
-#           "team": "TV63QKAAU"
-#       },
-#       "is_admin": false,
-#       "is_owner": false,
-#       "is_primary_owner": false,
-#       "is_restricted": false,
-#       "is_ultra_restricted": false,
-#       "is_bot": false,
-#       "is_app_user": false,
-#       "updated": 0
-#   },
-# {
-#   "id": "UUT9Z80AE",
-#   "team_id": "TV63QKAAU",
-#   "name": "thenora",
-#   "real_name": "Nora",
-#   "tz": "America\/Los_Angeles",
-#   "tz_label": "Pacific Daylight Time",
-#   "tz_offset": -25200,
-#   "profile": {
-#       "title": "",
-#       "phone": "",
-#       "skype": "",
-#       "real_name": "Nora",
-#       "real_name_normalized": "Nora",
-#       "display_name": "",
-#       "display_name_normalized": "",
-#       "status_text": "",
-#       "status_emoji": "",
-#       "status_text_canonical": "",
-#       "team": "TV63QKAAU"
-#   },
-#   "is_admin": true,
-#   "is_owner": true,
-#   "is_primary_owner": true,
-#   "is_restricted": false,
-#   "is_ultra_restricted": false,
-#   "is_bot": false,
-#   "is_app_user": false,
-#   "updated": 1583868865
-# },
