ada062dd1dcff280fe00b4a545b830aeb69e41a5
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-15 19:27:15 -0700

    Channels.rb details method and error handling

--- lib/channels.rb
+++ lib/channels.rb
@@ -1,7 +1,4 @@
 require_relative "recipient"
-require "httparty"
-require "dotenv"
-require "table_print"
 
 class Channel < Recipient
   attr_accessor :topic, :member_count
@@ -14,20 +11,28 @@ class Channel < Recipient
 
   # called in the workspace
   # in this list channel method we are calling on the Recipient
+
+  def details
+    return "Channel name: #{name}
+     Slack ID: #{slack_id}
+     Topic: #{topic}
+     Member count: #{member_count}"
+  end
+
   def self.list_channels
     response = Recipient.get("channels.list")
-    channel_list = []
-    if response["ok"] != true
-      raise SlackError, "There was an error. The error message is #{response["error"]}"
-    else
-      response["channels"].each do |channel|
-        @slack_id = channel["id"]
-        @name = channel["name"]
-        @topic = channel["topic"]
-        @member_count = channel["members"].length
 
-        channel_list << Channel.new(@slack_id, @name, @topic, @member_count)
-      end
+    unless response["ok"] == true
+      raise SlackError, "There was an error. The error message is #{response["error"]}"
+    end
+
+    channel_list = response["channels"].map do |channel|
+      slack_id = channel["id"]
+      name = channel["name"]
+      topic = channel["topic"]
+      member_count = channel["members"].length
+
+      Channel.new(slack_id, name, topic, member_count)
     end
     return channel_list
   end
