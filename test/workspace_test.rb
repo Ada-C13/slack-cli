471b00116ac78979d1b8a9f347672c48a6902015
dnguye2 <dnguyen1093@gmail.com>
2020-03-15 20:37:05 -0700

    fixed workspace_test so it could be used offline

--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -3,8 +3,26 @@ require_relative '../lib/workspace.rb'
 
 describe "Workspace" do
   before do
-    VCR.configuration.ignore_request { true }
-    @workspace = Workspace.new
+    VCR.use_cassette("users-list-endpoint") do
+      VCR.use_cassette("channels-list-endpoint") do
+        @workspace = Workspace.new
+      end
+    end
+  end
+
+  describe "Workspace instantiation" do
+    it "is an instance of Workspace" do
+      expect(@workspace).must_be_kind_of Workspace
+    end 
+
+    it "is set up for specific attributes" do
+      expect(@workspace.channels).must_be_kind_of Array
+      expect(@workspace.users).must_be_kind_of Array
+    end
+
+    it "has default status of nil for selected" do
+      expect(@workspace.selected).must_equal nil
+    end
   end
 
   describe "select_user" do
@@ -18,7 +36,7 @@ describe "Workspace" do
       expect(@workspace.select_user).must_equal "slackbot has been selected"
     end
 
-    it "raises an error if user provided is not valid" do
+    it "provides a message if a user could not be found" do
       def @workspace.get_user_input; "notauser" end
       expect(@workspace.select_user).must_equal "User could not be found."
     end
@@ -35,9 +53,13 @@ describe "Workspace" do
       expect(@workspace.select_channel).must_equal "random has been selected"
     end
 
-    it "raises a message if a channel could not be found" do
+    it "provides a message if a channel could not be found" do
       def @workspace.get_user_input; "notachannel" end
       expect(@workspace.select_channel).must_equal "Channel could not be found."
     end
+  end
+
+  describe "show_details" do
+    
   end
 end
