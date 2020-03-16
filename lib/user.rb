64f7a57b0ceb003c4ae5f2063f8174548e214a19
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-15 21:45:58 -0700

    Create tests for User class. Tests passing.

--- lib/user.rb
+++ lib/user.rb
@@ -3,30 +3,31 @@ require_relative 'recipient'
 module SlackApi
   class User < Recipient
 
-    attr_reader :real_name, :slack_id, :name
+    attr_reader :real_name, :slack_id, :name, :status_text, :status_emoji
 
     def initialize(
       slack_id:,
       name:,
-      real_name:
+      real_name:, 
+      status_text: nil,
+      status_emoji: nil
     )
 
     super(slack_id: slack_id, name: name)
 
 
       @real_name = real_name
-      # @status_text = status_text
-      # @status_emoji = status_emoji 
+      @status_text = status_text
+      @status_emoji = status_emoji
 
     end # initialize end
 
 
     # def show_details
-    # end
+  #   end
+    end #class end
     
-  
-
-  end #class end
+   
   
 end #module end
 
---
+++ test/user_test.rb
@@ -0,0 +1,19 @@
+require_relative "test_helper"
+
+
+describe "User Class" do
+  before do
+    VCR.use_cassette("new_user") do
+      @user = SlackApi::User.new(slack_id: 1, real_name: "char", name: "monkey")
+    end 
+  end
+
+  it "Creates an instance of User" do
+    expect(@user).must_be_kind_of SlackApi::User
+    expect(@user.slack_id).must_be_kind_of Integer
+    expect(@user.real_name).must_be_kind_of String
+    expect(@user.name).must_equal "monkey"
+
+
+  end
+end 
