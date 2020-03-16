cc3f8bc5b4a1c2376028fcf993340305d138a315
Yaz <yasmineoshaugh@gmail.com>
2020-03-15 22:10:18 -0700

    wrote Channel#init and Channel.get_list, with passing tests.

---
+++ lib/channel.rb
@@ -0,0 +1,22 @@
+require_relative 'recipient.rb'
+class Channel < Recipient
+  attr_reader :topic, :member_count
+  def initialize(name, slack_id, topic, member_count)
+    super(name, slack_id)
+    @topic = topic
+    @member_count = member_count
+  end
+
+  def self.get_list(recipient_type)
+    # whyyyyy doesn't super.get_list work??
+    # whhhyyyyy can't I implement a parameter-less version of get_list when i include a get_list matching 
+    # parent's method signature???
+    # whyyyyyyy
+    response = Recipient.get_list(recipient_type)
+    list = []
+    response.parsed_response["channels"].each do |channel|
+      list << Channel.new(channel["name"], channel["id"], channel["topic"]["value"], channel["num_members"]) 
+    end
+    return list
+  end
+end
---
+++ test/channel_test.rb
@@ -0,0 +1,50 @@
+require_relative 'test_helper.rb'
+
+describe 'Channel' do 
+  describe 'constructor' do
+    before do
+      @channel = Channel.new("general", "W012A3CDE", "gen happenings", 3)
+    end
+    it 'creates channel, child of recipient' do
+      expect(@channel).must_be_kind_of Channel
+      expect(@channel.class.superclass).must_equal Recipient
+    end
+    it "has expected state" do
+      expect(@channel.name).must_equal "general"
+      expect(@channel.slack_id).must_equal "W012A3CDE"
+      expect(@channel.topic).must_equal "gen happenings"
+      expect(@channel.member_count).must_equal 3 
+    end
+
+  end
+
+  describe "get_list" do
+    # TO DO: before block with cassette
+    it "returns array" do
+      VCR.use_cassette("Channel.get_list") do
+        channel_list = Channel.get_list("channel")
+        expect(channel_list).must_be_kind_of Array
+        expect(channel_list.empty?).must_equal false
+        expect(channel_list.first).must_be_kind_of Channel
+      end
+    end
+
+    it "has channel called Random" do
+      VCR.use_cassette("Channel.get_list") do
+        channel_list = Channel.get_list("channel")
+        target_channel = channel_list.find {|channel| channel.name == "random"}
+        expect(target_channel).must_be_kind_of Channel
+      end
+    end
+
+    it "channel SlackBot has expected state" do
+      VCR.use_cassette("Channel.get_list") do
+        channel_list = Channel.get_list("channel")
+        target_channel = channel_list.find {|channel| channel.name == "random"}
+        expect(target_channel.slack_id).must_equal "CVBCU0R37"
+        expect(target_channel.topic).must_equal "Non-work banter and water cooler conversation"
+        expect(target_channel.member_count).must_equal 1
+      end
+    end
+  end
+end
