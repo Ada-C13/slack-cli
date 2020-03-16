04980015eca3d7ce5dc393e84e1127d6bbcd2df5
Angela Nguyen <angethuy@gmail.com>
2020-03-16 05:46:07 -0700

    workspace scaffold: requires, methods, NoRecepientError

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,14 +1,42 @@
+require "dotenv"
+require "httparty"
+require_relative "conversation"
+require_relative "user"
+
+Dotenv.load
+
+SLACK_TOKEN = ENV["SLACK_TOKEN"]
+
+
 module Slack
   class Workspace
-    attr_reader :users, :channels
+    attr_reader :users, :channels, :selected
     
     def initialize
-      @users = []
-      @channels = []
+      @users = User.get_all
+      @channels = Channels.get_all # TO-DO: replace this variable with CONVERSATIONS and create methods to list by type of conversation
+      @selected = nil
     end
 
+    # Method selects a user or channel using the name or slack ID. 
+    def select_conversation
+    end
 
+    # Method shows details of the currently selected conversation.
+    def show_details
+      raise NoRecipientError, "Cannot display details."
+    end
 
+    # Method posts a message to the currently selected conversation.
+    def post_message
+      raise NoRecipientError, "Cannot send message."
+    end
 
+  end
+
+  class NoRecipientError < StandardError
+    def initialize(msg="No currently selected user or conversation.")
+      super
+    end
   end
 end
