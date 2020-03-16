af02fe5d9da1437438aef495d7a8bc9cdf416cbf
Yaz <yasmineoshaugh@gmail.com>
2020-03-15 22:37:33 -0700

    Workspace#init with tests for #init and #channels, #users - all passing

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,7 +1,11 @@
+require_relative "lib_helper.rb"
 class Workspace
+
   attr_reader :users, :channels
   def initialize
-    @users = []
-    @channels = []
+    @users = User.get_list("user")
+    @channels = Channel.get_list("channel")
   end
+
+ 
 end
--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -1,9 +1,11 @@
 require_relative 'test_helper'
 
-describe 'Workspace' do 
+xdescribe 'Workspace' do 
   describe 'constructor' do
     before do
-      @workspace = Workspace.new
+      VCR.use_cassette("workspace") do
+        @workspace = Workspace.new
+      end
     end
     it 'creates instance of Workspace' do
       expect(@workspace).must_be_kind_of Workspace
@@ -11,6 +13,17 @@ describe 'Workspace' do
     it 'has state @users and @channels' do
       expect(@workspace.users).must_be_kind_of Array
       expect(@workspace.channels).must_be_kind_of Array
+    end
+    it "users contains users" do
+      expect(@workspace.users.first).must_be_kind_of User
+      target_user = @workspace.users.find {|user| user.name == "slackbot"}
+      expect(target_user).must_be_kind_of User
+    end
+
+    it "users contains users" do
+      expect(@workspace.channels.first).must_be_kind_of Channel
+      target_channel = @workspace.channels.find {|channel| channel.name == "random"}
+      expect(target_user).must_be_kind_of Channel
     end
   end
 end
