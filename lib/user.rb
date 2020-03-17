e480f3c50d57294467003b1c2c1a3448c16df094
Angela Nguyen <angethuy@gmail.com>
2020-03-17 01:32:20 -0700

    added User init method tests and code

--- lib/user.rb
+++ lib/user.rb
@@ -1,25 +1,41 @@
+require "dotenv"
+require "httparty"
+
+Dotenv.load
+
+SLACK_TOKEN = ENV["SLACK_TOKEN"]
+
 module Slack
   class User
+    attr_reader :id, :user_name, :real_name
     
-    def initialize(user)
-      @id
-      @user_name
+    def initialize(member)
+      @id = member["id"]
+      @user_name = member[""]
       @real_name
     end
 
 
     # CLASS METHODS
 
-    # Method uses http get to retrieve all User "objects"
-    def self.get_all
-      # httparty GET 
-      # API endpoint https://slack.com/api/users.list?
-    end
-
-    # Method takes the results of get_all and pretty-prints them.
+    # Parameter users: collection representing Users
+    # Returns an array of User objects 
+    
     def self.list_all
+      members = get_all
+      members.map { |member| User.new(member) }
+
 
     end
 
+    private
+
+    # Method uses http get to retrieve all User "objects"
+    # returns an httparty Response object
+    def self.get_all
+      data = httparty.get(https://slack.com/api/users.list?, query: { token: SLACK_TOKEN, })
+      raise StandardError, "Users.list endpoint response IS NOT OK."
+      return data["members"]
+    end
   end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -3,13 +3,51 @@ require_relative "../lib/user"
 
 describe "user" do
   describe "initialize" do
-    # creates a user object
-    # user returns an id
-    # user has a user_name
+    before do 
+      test_member = 
+      {
+        "id": "UV614256C",
+        "team_id": "TV5H57Z7E",
+        "name": "angethuy",
+        "is_bot": false,
+        "color": "9f69e7",
+        "real_name": "Angela",
+      }
+      user = User.new(test_member)
+    end
+
+    it "raises an exception when trying to create a User with bad parameters" do
+      expect{(User.new("pumpkin pie"))}.must_raise ArgumentError
+    end
+
+    it "creates a User object" do
+      expect(user).is_kind_of? Slack::User
+    end
+
+    it "returns a slack id" do
+      expect(user.id).must_equal "UV614256C"
+    end
+
+    it "returns a username" do
+      expect(user.name).must_equal "angethuy"
+    end
+
+    it "returns a real name" do
+      expect(user.real_name).must_equal "Angela"
+    end
   end
 
   describe "self.get_all" do
     # raise exception for bad endpoint response
+    # API endpoint https://slack.com/api/users.list?
+    it "successfully receives a response from the users.list endpoint" do
+      users = {}
+      VCR.use_cassette(users_list_endpoint) do
+        users = User.get_all
+      end
+
+      expect(users)
+    end
   end
 
   describe "self.list_all" do
